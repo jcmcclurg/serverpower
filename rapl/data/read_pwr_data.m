64406595a65eef9f6472ede0b10d64229ad8feb1
powerserver <powerserver@jjpowerserver.dynamic.uiowa.edu>
2015-06-01 16:57:50 -0500

    added and revised convert_timestamp.sh and read_pwr_data.m

---
+++ rapl/data/read_pwr_data.m
@@ -0,0 +1,43 @@
+%read_pwr_data.m
+close all;
+clear all;
+clc;
+%t_lv = csvread('data_4_15_15.csv','B2:B911');
+%p_lv = csvread('data_4_15_15.csv','A2:A911');
+%t_pg = csvread('data_4_15_15.csv','C2:C1072');
+%p_pg = csvread('data_4_15_15.csv','D2:D1072');
+%p_lv_ave = csvread('data_4_15_15.csv','G2:G911');
+%p_lv_ave_ten = csvread('data_4_15_15.csv','H2:H911');
+
+% open file and get data
+fid = fopen('pver.csv');
+data=textscan(fid,'%f %f %f %f %f %f','Delimiter',',','HeaderLines',1);
+fclose(fid);
+fid = fopen('setpt.csv');
+data2=textscan(fid,'%f %f','Delimiter',',','HeaderLines',0);
+fclose(fid);
+% save data as readable variables
+t_lv=data{1};
+p_lv=data{2}*200.; % I*20V*10(gain-scaling)
+t_pg=data{3};
+p_pg=data{4};
+tmp0_pg=data{5};
+t_sp=data2{1};
+p_sp=data2{2};
+
+%plot data
+fh=figure();
+
+plot(t_pg,p_pg,'r:.','displayname','power registers');
+hold on;
+grid on;
+%plot(t_lv,p_lv,'b:.','displayname','power meter');
+stairs(t_sp,p_sp,'g--','displayname','set point');
+%plot(t_lv,p_lv_ave,'g','displayname','power meter ave (over 5)');
+%plot(t_lv,p_lv_ave_ten-10,'g:.','displayname','power meter ave (over 10) - 10W');
+title('Measured (current sensor) & Estimated (processor counters) Power');
+xlabel('Time (seconds since 00:00 hours)');
+ylabel('Power (watts)');
+legend();
+%PrettyPlot(fh);
+saveas(fh,"read_pwr.pdf");
--- rapl/powerColumn.sh
+++ rapl/powerColumn.sh
@@ -2,5 +2,5 @@
 
 #./power_gadget -e 500 | cut -d ',' -f1
 # for some reason, cut buffers the output, so you have to turn that off
-stdbuf -i0 -o0 -e0 sudo ./power_gadget -e 500 | stdbuf -i0 -o0 -e0 cut -d ',' -f1,3-5,8,11,14 | tee data/data.csv | stdbuf -i0 -o0 -e0 cut -d ',' -f4,5
+stdbuf -i0 -o0 -e0 sudo ./power_gadget -e 40 | tee data/data.csv | stdbuf -i0 -o0 -e0 cut -d ',' -f3
 #sudo ./power_gadget -e 1000 | stdbuf -i0 -o0 -e0 cut -d ',' -f5
