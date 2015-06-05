ca8e50c8db9630317db7ea040f902d1ffb2c2a7e
Joe <joseph-hall@uiowa.edu>
2015-06-04 19:01:56 -0500

    delay test and data plotting scripts finished

--- sleepdelay/data/convert_timestamp.sh
+++ sleepdelay/data/convert_timestamp.sh
@@ -10,9 +10,15 @@ awk -F, '{printf("%d,\n",$6/1000)}' data.csv | tee thermalzns.csv #> /dev/null
 awk -F [::.,] '{printf("%.16G\n",($1*3600+$2*60+$3+$4/1000))}' setpoint.csv | tee time_sp.csv #> /dev/null
 cut -d, -f2 setpoint.csv | tee power_sp.csv #> /dev/null
 
+# convert delay data
+#!/bin/bash
+awk -F [::.,] '{printf("%.16G\n",($1*3600+$2*60+$3+$4/1000000))}' delay.csv | tee time_dl.csv
+cut -d, -f2 delay.csv | tee delay_dl.csv
+
 # now paste together with:
 
 paste -d ',' time_lv.csv power_lv.csv time_pg.csv power_pg.csv thermalzns.csv > pver.csv #> /dev/null
 paste -d ',' time_sp.csv power_sp.csv > setpt.csv #> /dev/null
+paste -d ',' time_dl.csv delay_dl.csv > dly.csv
 
-octave read_pwr_data.m
+#octave read_pwr_data.m
--- sleepdelay/test_delay_v_power.c
+++ sleepdelay/test_delay_v_power.c
@@ -7,21 +7,24 @@
 #include <sys/time.h>
 #include <time.h>
 #include <signal.h>
+#include <math.h>
 
 void convert_time_to_string(struct timespec tv, char* time_buf);
 
 int main (void) {
 
-	double delay_sec = 0.1;
+	double delay_sec = 1.0;
 	int delay_us = delay_sec*1000000;
-	double num_steps = 1000; // number of step inputs
-	double dmax = 0.1;
-	double dmin = 0.0;
+	double num_steps = 30; // number of step inputs
+	double dmax = 0.0001;
+	double dmin = 0.000000001;
     double step_height = (dmax-dmin)/num_steps;
-	int i;
+	double i;
 
+	fprintf(stdout, "minimum delay set as: %.18f\n",dmin); 
 	for (i=0;i<num_steps+1;i++){
-		fprintf(stdout, "%.2f\n",(double)i*step_height+dmin); 
+		//fprintf(stdout, "%.18f\n",(double)i*step_height+dmin); 
+		fprintf(stdout, "%.18f\n",pow(2.0,i)*dmin); 
 		usleep(delay_us);
 	}
 	fprintf(stdout, "quit pwrtest.c\n"); 
