ee88c5e319fcd1fa3de31e856dbb603f46d5a58d
Josiah McClurg <jcmcclurg@gmail.com>
2015-06-16 23:23:30 -0500

    Updated power output plotter

--- signal_insert_delays/power_output/power_output_plotter.m
+++ signal_insert_delays/power_output/power_output_plotter.m
@@ -1,7 +1,41 @@
-b="737849824_1434205397";
-a = dlmread([b ".output"]," ");
+b=input('Type the name: ')
+%b='253705126_1434500446';
+
+[status, output] = system(['cat ./' b '.log | grep "Starting power gadget at " | cut -d \  -f 5']);
+assert(status == 0);
+startTime = str2double(output);
+
+[status, output] = system(['cat ./' b '.log | grep "Step to " | cut -d \  -f 3']);
+assert(status == 0);
+output = strsplit(output);
+stepValues = str2double(output);
+stepValues = stepValues(!isnan(stepValues));
+
+% Normalize the times to when the power gadget started.
+[status, output] = system(['cat ./' b '.log | grep "Step to " | cut -d \  -f 5']);
+assert(status == 0);
+output = strsplit(output);
+stepTimes = str2double(output) - startTime;
+
+% Normalize the times to when the power gadget started.
+a = dlmread(['./' b '.output'],' ');
 a(:,1) = a(:,1) - a(1,1);
 badTimes = a(:,1) < 0;
 a(badTimes,:) = [];
 
-plot(a(:,1),a(:,2))
+riseTime = 0.1;
+
+windows = {};
+numSteps = (length(stepTimes)-1);
+for i = 1:numSteps
+	window = (a(:,1) >= (stepTimes(i)+riseTime)) & (a(:,1) < (stepTimes(i+1)-riseTime));
+	windows(i) = a(window,2);
+	%plot(a(window,1),a(window,2))
+end
+
+[v,i] = sort(stepValues);
+boxplot(windows(i(1:numSteps)));
+set(gca(),'xtick', 1:numSteps, 'xticklabel', v(1:numSteps)*100 );
+title('CPU duty cycle vs average power')
+ylabel('Power (W) ')
+xlabel('Duty cycle (percent)')
