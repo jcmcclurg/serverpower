142fae4843e40ea20462a0ae0502663c73287f70
Josiah McClurg <jcmcclurg@gmail.com>
2015-06-18 21:54:30 -0500

    Did some modifications

--- hypervisor/hypervisor.sh
+++ hypervisor/hypervisor.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
 echo Welcome to the hypervisor cpu limiter. >&2
-echo Type the limiting values. Press CTRL+C to exit. >&2
+echo Type the limiting values. Press CTRL+C or q to exit. >&2
 echo The range is 0-400. See xl sched-credit -c for details >&2
-while true; do
+
+dom=${1:-Domain-0}
+rangeMax=400
+rangeMin=1
+
+limitVal=400
+
+trap "sudo xl sched-credit -d $dom -c $rangeMax; echo 'Reset credits to $rangeMax on exit.' >&2; exit;" SIGINT SIGTERM
+
+while [[ "$limitVal" != "q" ]]; do
+	if [ $limitVal -lt $rangeMin ]; then
+		limitVal=$rangeMin
+	elif [ $limitVal -gt $rangeMax ]; then
+		limitVal=$rangeMax
+	fi
+	sudo xl sched-credit -d $dom -c $limitVal
+	echo "Set credits to $limitVal" >&2
 	read limitVal
-	sudo xl sched-credit -d Domain-0 -c $limitVal
 done
+echo "Done" >&2
