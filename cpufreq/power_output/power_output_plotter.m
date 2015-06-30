6f71f31e61b496ab9a515c7abf93ecae234db759
Josiah McClurg <jcmcclurg@gmail.com>
2015-06-30 13:07:51 -0500

    Added cpufreq modules

---
+++ cpufreq/power_output/power_output_plotter.m
@@ -0,0 +1,47 @@
+b=input('Type the name: ','s')
+%b='562281316_1435616496';
+
+[status, output] = system(['cat ./' b '.log | grep "Starting power gadget at " | cut -d \  -f 5']);
+assert(status == 0);
+startTime = str2double(output);
+
+[status, output] = system(['cat ./' b '.log | grep "Step to " | cut -d \  -f 3']);
+assert(status == 0);
+output = strsplit(output);
+stepValues = str2double(output);
+stepValues = stepValues(!isnan(stepValues));
+
+% Normalize the times to when the power gadget started.
+[status, output] = system(['cat ./' b '.log | grep "Step to " | cut -d \  -f 5']);
+assert(status == 0);
+output = strsplit(output);
+stepTimes = str2double(output) - startTime;
+
+% Normalize the times to when the power gadget started.
+a = dlmread(['./' b '.output'],' ');
+a(:,1) = a(:,1) - a(1,1);
+badTimes = a(:,1) < 0;
+a(badTimes,:) = [];
+
+riseTime = 0.1;
+
+windows = {};
+numSteps = (length(stepTimes)-1);
+for i = 1:numSteps
+	window = (a(:,1) >= (stepTimes(i)+riseTime)) & (a(:,1) < (stepTimes(i+1)-riseTime));
+	windows(i) = a(window,2);
+	%plot(a(window,1),a(window,2))
+end
+
+[v,i] = sort(100-stepValues);
+plotI = i(1:10:numSteps);
+plotV = v(1:10:numSteps);
+boxplot(windows(plotI));
+ticks = unique([1:5:length(plotI) length(plotI)]);
+
+set(gca(),'xtick', ticks, 'xticklabel', plotV(ticks) );
+title('CPU duty cycle vs average power')
+ylabel('Power (W) ')
+xlabel('Duty cycle (percent)')
+print([b '.png'],'-S1280x1024')
+print('powerclamp.png','-S1280x1024')
