dacb9a3631948823630634bcdc6dc1e50b73d953
Joe <joseph-hall@uiowa.edu>
2015-07-15 14:10:14 -0500

    fixed to_rms.m
    Former-commit-id: c51f39a4f02e44cebdf34891e6a7a3347145da6f [formerly c51f39a4f02e44cebdf34891e6a7a3347145da6f [formerly 14b2dc19d7db3424a356dc283eea75ee85b95e05]]
    Former-commit-id: f932bab0c8123746430fa1c90f7a48347b9c13b2
    Former-commit-id: ffd06975b041b71165e72dc03b6a5c26e19c24ed

--- rapl/data/to_rms.m
+++ rapl/data/to_rms.m
@@ -1,3 +1,4 @@
+% to_rms.m
 load data_scope.mat
 t_iac = data_scope(1:end,1);
 iac = data_scope(1:end,2);
@@ -82,49 +83,10 @@ pcnt_err2=(prms_ave-tot_interp-ave_base2)./prms_ave*100;
 fh=figure;
 %h2=plot(t_iac,prms,'b','displayname','RMS Power (running period window)');
 
-h1=plot(t_ave,prms_ave,'or','markersize',5,'displayname','RMS Average Power');
+h1=plot(t_ave,prms_ave,'or','markersize',2,'displayname','RMS Average Power');
 hold on;
-h3=plot(t_ave,p_pg_interp,'.m','markersize',5,'displayname','PKG MSR');
-%h4=plot(t_pg,tot_pg,':.b','markersize',5,'displayname','PKG+DRAM MSR');
-%h5=plot(t_aveInst,pinst_ave,':.m','markersize',5,'displayname','Instantaneous Average (0.1s) Power');
-<<<<<<< HEAD
-h6=plot(t_ave,p_base,'>b','markersize',10,'displayname','AveRMS-msrPKG');
-h7=plot(t_ave,p_base2,'sg','markersize',10,'displayname','AveRMS-msrPKG-msrDRAM');
-h8=plot(xlim,[mean(prms_ave(2:30)-p_pg_interp(2:30)) mean(prms_ave(end-30:end-1)-p_pg_interp(end-30:end-1))],'-b','displayname','mean of AveRMS-msrPKG');
-h9=plot(xlim,[mean(prms_ave(2:30)-tot_interp(2:30)) mean(prms_ave(end-30:end-1)-tot_interp(end-30:end-1))],'-g','displayname','mean of AveRMS-msrPKG-msrDRAM');
-h10=plot(t_ave,pcnt_err2,'sg','markersize',10,'displayname','pcentErr2');
-h11=plot(t_ave,pcnt_err,'>b','markersize',10,'displayname','pcentErr1');
-L1 = legend;
-=======
-h6=plot(t_ave,p_base,'>b','markersize',2,'displayname','AveRMS-msrPKG');
-h7=plot(t_ave,p_base2,'sg','markersize',2,'displayname','AveRMS-msrPKG-msrDRAM');
-%h8=plot(xlim,[mean(prms_ave(2:30)-p_pg_interp(2:30)) mean(prms_ave(end-30:end-1)-p_pg_interp(end-30:end-1))],'-b','displayname','mean of AveRMS-msrPKG');
-%h9=plot(xlim,[mean(prms_ave(2:30)-tot_interp(2:30)) mean(prms_ave(end-30:end-1)-tot_interp(end-30:end-1))],'-g','displayname','mean of AveRMS-msrPKG-msrDRAM');
-h10=plot(t_ave,pcnt_err2,'.g','markersize',5,'displayname','pcentErr2');
-h11=plot(t_ave,pcnt_err,'.b','markersize',5,'displayname','pcentErr1');
-L1 = legend('show');
->>>>>>> 23a97f6a4c63e3271ba7c6ac23beb83939619832
-xlabel('Time (seconds)');
-ylabel('Power (W)');
-FS1=findall(gca,'-property','Fontsize');
-set(FS1, "fontsize", 12, "linewidth", 2);
-grid on;
-FL1= findall(L1,'-property','FontSize');
-set(L1,'FontSize',12,'location','northwest');
+h3=plot(t_pg,p_pg,'.m','markersize',5,'displayname','PKG MSR');
 
-fh2=figure;
-ha=plot(p_pg_interp,prms_ave,'>b','markersize',10,'displayname','Package Counter');
-hold on;
-hb=plot(tot_interp,prms_ave,'sg','markersize',10,'displayname','Package+DRAM Counter');
-L2 = legend;
-xlabel('MSR Energy Counter Processor Power Estimate (W)');
-ylabel('Measured Server Power (rms) (W)');
-FS2=findall(gca,'-property','Fontsize');
-set(FS2, "fontsize", 12, "linewidth", 2);
-grid on;
-T2 = title({"Power (rms) vs Power (counter)"});
-FL2= findall(L2,'-property','FontSize');
-set(L2,'FontSize',12,'location','northwest');
 
 X=[prms_ave p_pg_interp tot_interp];
 csvwrite('forLSfit.csv',X);
