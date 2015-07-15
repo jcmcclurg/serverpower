d3fb7f76f0f49eaad99fde8a8fda84dad4309b8f
Joe <joseph-hall@uiowa.edu>
2015-07-15 13:58:34 -0500

    added scripts in pmstresstest4 which can reproduce plots
    Former-commit-id: fb1fadc378565d16a89b0252afb60011c3ce66e2 [formerly fb1fadc378565d16a89b0252afb60011c3ce66e2 [formerly 7e923338b9856a09e43faa06ed8c62d94a3b20ba]]
    Former-commit-id: a32e605668e0cdfe8e49513b7f0df8f2cb0bfa4a
    Former-commit-id: d0f4e36352e3caf5fad9dea27ec9b5dfbd04f495

---
+++ rapl/data/r320/pmstresstest4/rmsVmsr_script.m
@@ -0,0 +1,15 @@
+% first run to_rms.m and msr_error_script.m
+
+fh2=figure;
+ha=plot(p_pg_interp,prms_ave,'>b','markersize',10,'displayname','Package Counter');
+hold on;
+hb=plot(tot_interp,prms_ave,'sg','markersize',10,'displayname','Package+DRAM Counter');
+L2 = legend;
+xlabel('MSR Energy Counter Processor Power Estimate (W)');
+ylabel('Measured Server Power (rms) (W)');
+FS2=findall(gca,'-property','Fontsize');
+set(FS2, "fontsize", 12, "linewidth", 2);
+grid on;
+T2 = title({"Power (rms) vs Power (counter)"});
+FL2= findall(L2,'-property','FontSize');
+set(L2,'FontSize',12,'location','northwest');
