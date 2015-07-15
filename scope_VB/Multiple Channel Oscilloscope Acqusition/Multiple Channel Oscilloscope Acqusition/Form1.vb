dc4ea9a42b42c8fed23b7c7408a817bc5ed43e7a
AWynos <WynosAydan@gmail.com>
2015-07-15 10:20:47 -0500

    Multi Channel Oscilloscope Acquisition
    Fully functional
    
    
    Former-commit-id: efc445b9a75ad324ce355511cedf62c873fecba1 [formerly be7860af69c0f0459160f8beb3f1ec28842973e3] [formerly be7860af69c0f0459160f8beb3f1ec28842973e3 [formerly 3777b731f81658a6fef7121972793e18b5e58edc]]
    Former-commit-id: 03f55d22f2b807b3c56f446edede8a6740f27673
    Former-commit-id: 3806970b1b09c3784a216aad65f0cedae30f0200

--- scope_VB/Multiple Channel Oscilloscope Acqusition/Multiple Channel Oscilloscope Acqusition/Form1.vb
+++ scope_VB/Multiple Channel Oscilloscope Acqusition/Multiple Channel Oscilloscope Acqusition/Form1.vb
@@ -35,6 +35,46 @@ Public Class FRM1
 
     End Sub
 
+    Private Sub setChan1()
+
+        Dim chanRange As String
+
+        chanRange = TXT1.Text * 8
+
+        myScope.WriteString(":Channel1:Range " + chanRange)
+
+    End Sub
+
+    Private Sub setChan2()
+
+        Dim chanrange As String
+
+        chanrange = TXT1.Text * 8
+
+        myScope.WriteString(":Channel2:Range " + chanrange)
+
+    End Sub
+
+    Private Sub setChan3()
+
+        Dim chanRange As String
+
+        chanRange = TXT1.Text * 8
+
+        myScope.WriteString(":Channel3:Range " + chanRange)
+
+    End Sub
+
+    Private Sub setChan4()
+
+        Dim chanRange As String
+
+        chanRange = TXT1.Text * 8
+
+        myScope.WriteString(":Channel4:Range " + chanRange)
+
+    End Sub
+
     Private Sub FRM1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         myMgr = New ResourceManager
@@ -42,7 +82,7 @@ Public Class FRM1
 
         myScope.IO = myMgr.Open("USB0::2391::5965::MY50340366::INSTR")
 
-        scopeData = File.CreateText("C:\Users\AWynos\Desktop\Oscilloscope Program\ScopeData.txt")
+        'scopeData = File.CreateText("C:\Users\AWynos\Desktop\Oscilloscope Program\ScopeData.txt")
 
     End Sub
 
@@ -55,6 +95,7 @@ Public Class FRM1
         REM No ":Waveform:Source" command is required. The ":Digitize" command without specified channels will
         REM Digitize all channels that are currently displayed. The initialize button will take care of which channels
         REM are to be displayed.
+
         myScope.WriteString(":Waveform:Format BYTE")
         myScope.WriteString(":Waveform:Points MAXimum")
 
@@ -80,58 +121,88 @@ Public Class FRM1
         Dim strOutput As String
         Dim timeRef As Double
         Dim timeOffset As Double
+        Dim Channel(4)
+        Dim filePath As String
+        Dim i As Integer
 
-        'myScope.WriteString(":WAVEFORM:SOURCE CHAN" + chanNum) 'sets source to channel 1
+        For i = 1 To 4
+            Channel(i) = -1
+        Next
 
-        myScope.WriteString(":WAVEFORM:POINTS:MODE Normal")
-        myScope.WriteString(":WAVEFORM:POINTS MAXimum") 'sets points to MAX
+        If CHK1.Checked = True Then
+            Channel(1) = 1
+        End If
+        If CHK2.Checked = True Then
+            Channel(2) = 2
+        End If
+        If CHK3.Checked = True Then
+            Channel(3) = 3
+        End If
+        If CHK4.Checked = True Then
+            Channel(4) = 4
+        End If
 
-        Dim lngVSteps As Long
-        Dim intBytesPerData As Integer
+        For i = 1 To 4
 
-        myScope.WriteString(":WAVEFORM:FORMAT BYTE") 'sets data format to BYTE
-        lngVSteps = 65536
-        intBytesPerData = 1
+            If Channel(i) <> -1 Then
 
-        myScope.WriteString(":WAVEFORM:PREAMBLE?") 'Query for the preamble.
+                filePath = "C:\Users\AWynos\Desktop\Oscilloscope Program\Chan" + i.ToString + ".txt"
+                scopeData = File.CreateText(filePath) 'Creates a file for each channel being digitized
 
-        Preamble = myScope.ReadList     'Reads preamble information and places it into an array
-        intFormat = Preamble(0)         'Format: 0 = Byte, 1 = Word, 4 = ASCII
-        intType = Preamble(1)           'Type: 0 = Normal, 1 = Peak, 2 = Average
-        lngPoints = Preamble(2)         'Points: Number of data points transfered
-        lngCount = Preamble(3)          'Will always be 1
-        dblXIncrement = Preamble(4)     'Time difference between data points
-        dblXOrigin = Preamble(5)        'Always the first data point in memory
-        lngXReference = Preamble(6)     'Specifies the data point associated with X-Origin
-        sngYIncrement = Preamble(7)     'Voltage difference between data points
-        sngYOrigin = Preamble(8)        'Value is the voltage at center screen
-        lngYReference = Preamble(9)     'Specifies the data point where y-origin occurs
-        strOutput = ""                  'Variable holding final string output
+                myScope.WriteString(":WAVEFORM:SOURCE CHAN" + i.ToString) 'Sets channel source
 
-        myScope.WriteString(":WAV:DATA?") 'Query the scope for waveform data
+                myScope.WriteString(":WAVEFORM:POINTS:MODE Normal")
+                myScope.WriteString(":WAVEFORM:POINTS MAXimum") 'Sets points to max
 
-        Dim lngI As Long
-        Dim lngDataValue As Long
+                Dim lngVSteps As Long
+                Dim intBytesPerData As Integer
 
-        timeOffset = (dblXOrigin - lngXReference * dblXIncrement)
-        scopeData.WriteLine("Waveform data:" + vbCrLf + strOutput) 'Writes Preamble information to scopeData
-        scopeData.WriteLine()
-        varQueryResult = myScope.ReadIEEEBlock(IEEEBinaryType.BinaryType_UI1) 'Reads buffered waveform data from the scope in a binary block
-        scopeData.WriteLine("Volts, Time (seconds)")
-        For lngI = 0 To UBound(varQueryResult)
-            If (lngI Mod 1) = 0 Then
-                strOutput = "" 'Resets strOutput to clear memory
-                lngDataValue = varQueryResult(lngI) ' 8-bit value
-                strOutput += FormatNumber((lngDataValue - lngYReference) * sngYIncrement + sngYOrigin, 5) + "," 'Shows voltage
-                REM Get the time reference from the scope
-                timeRef = ((lngI / intBytesPerData - lngXReference) * dblXIncrement + dblXOrigin)
-                timeStamp = timeRef - timeOffset + startTime
-                strOutput += Format(timeStamp, "#####.#########")
-                scopeData.WriteLine(strOutput) 'Writes data from current loop iteration to the data file
+                myScope.WriteString(":WAVEFORM:FORMAT BYTE") 'Sets format to bytes
+                lngVSteps = 65536
+                intBytesPerData = 1
+
+                myScope.WriteString(":WAVEFORM:PREAMBLE?") 'Query for the preamble.
+
+                Preamble = myScope.ReadList     'Reads preamble information and places it into an array
+                intFormat = Preamble(0)         'Format: 0 = Byte, 1 = Word, 4 = ASCII
+                intType = Preamble(1)           'Type: 0 = Normal, 1 = Peak, 2 = Average
+                lngPoints = Preamble(2)         'Points: Number of data points transfered
+                lngCount = Preamble(3)          'Will always be 1
+                dblXIncrement = Preamble(4)     'Time difference between data points
+                dblXOrigin = Preamble(5)        'Always the first data point in memory
+                lngXReference = Preamble(6)     'Specifies the data point associated with X-Origin
+                sngYIncrement = Preamble(7)     'Voltage difference between data points
+                sngYOrigin = Preamble(8)        'Value is the voltage at center screen
+                lngYReference = Preamble(9)     'Specifies the data point where y-origin occurs
+                strOutput = ""                  'Variable holding final string output
+
+                myScope.WriteString(":WAV:DATA?") 'Query the scope for waveform data
+
+                Dim lngI As Long
+                Dim lngDataValue As Long
+
+                timeOffset = (dblXOrigin - lngXReference * dblXIncrement) 'Finds the time difference between data points
+                scopeData.WriteLine("Data from Channel " + i.ToString) 'Writes which channel data is being captured from
+                scopeData.WriteLine("Waveform data:" + vbCrLf + strOutput) 'Writes Preamble information to scopeData
+                varQueryResult = myScope.ReadIEEEBlock(IEEEBinaryType.BinaryType_UI1) 'Reads buffered waveform data from the scope in a binary block
+                scopeData.WriteLine("Volts, Time (seconds)")
+                For lngI = 0 To UBound(varQueryResult)
+                    If (lngI Mod 1) = 0 Then
+                        strOutput = "" 'Resets strOutput to clear memory
+                        lngDataValue = varQueryResult(lngI) ' 8-bit value
+                        strOutput += FormatNumber((lngDataValue - lngYReference) * sngYIncrement + sngYOrigin, 5) + ", " 'Shows voltage
+                        REM Get the time reference from the scope
+                        timeRef = ((lngI / intBytesPerData - lngXReference) * dblXIncrement + dblXOrigin)
+                        timeStamp = timeRef - timeOffset + startTime 'Creates a timestamp for each data point
+                        strOutput += Format(timeStamp, "#####.#########") 'Formats data in the text file
+                        scopeData.WriteLine(strOutput) 'Writes data from current loop iteration to the data file
+                    End If
+                Next lngI
+
+                scopeData.Close()
+
             End If
-        Next lngI
-
-        scopeData.Close()
+        Next i
 
     End Sub
 
@@ -139,39 +210,35 @@ Public Class FRM1
 
         myScope.IO.Clear()
 
-        'myScope.WriteString("*RST")
-
-        myScope.WriteString(":Timebase:Range 10")
         myScope.WriteString(":Timebase:Delay 0")
-        myScope.WriteString(":Timebase:Reference Center")
-
-        'myScope.WriteString(":Channel" + chanNum + ":Probe 10")
-        'myScope.WriteString(":Channel" + chanNum + ":Range 8")
-        'myScope.WriteString(":Channel" + chanNum + ":Offset 0")
-        'myScope.WriteString(":Channel" + chanNum + ":Coupling DC")
-
+        myScope.WriteString(":Timebase:Reference Left")
         myScope.WriteString(":Acquire:Type Normal")
+        myScope.WriteString(":RUN")
 
         If CHK1.Checked = True Then
             myScope.WriteString(":Channel1:Display ON")
+            myScope.WriteString(":Channel1:Offset 0")
         Else
             myScope.WriteString(":Channel1:Display OFF")
         End If
 
         If CHK2.Checked = True Then
             myScope.WriteString(":Channel2:Display ON")
+            myScope.WriteString(":Channel2:Offset 0")
         Else
             myScope.WriteString(":Channel2:Display OFF")
         End If
 
         If CHK3.Checked = True Then
             myScope.WriteString(":Channel3:Display ON")
+            myScope.WriteString(":Channel3:Offset 0")
         Else
             myScope.WriteString(":Channel3:Display OFF")
         End If
 
         If CHK4.Checked = True Then
             myScope.WriteString(":Channel4:Display ON")
+            myScope.WriteString(":Channel4:Offset 0")
         Else
             myScope.WriteString(":Channel4:Display OFF")
         End If
@@ -181,67 +248,99 @@ Public Class FRM1
     Private Sub BTN5_Click(sender As Object, e As EventArgs) Handles BTN5.Click
 
         Dim rng As String, range As String
+        Dim m As Integer
 
-        rng = TXT2.Text * 10
+        Try
+            rng = TXT2.Text * 10
 
-        range = ":Timebase:Range " + rng
+            range = ":Timebase:Range " + rng
 
-        myScope.WriteString(range)
+            myScope.WriteString(range)
+        Catch ex As Exception
+            m = MessageBox.Show("Please enter a timebase range!", "No Range Entered", _
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            If m = Windows.Forms.DialogResult.OK Then
+                TXT2.Clear()
+                TXT2.Focus()
+                Exit Sub
+            End If
+        End Try
 
     End Sub
 
     Private Sub BTN4_Click(sender As Object, e As EventArgs) Handles BTN4.Click
 
-        Dim chanRng As Double, chanRange As String
-        Dim c As Integer, m As Integer
+        Dim m As Integer
+
+        If CHK1.Checked = False And CHK2.Checked = False And _
+            CHK3.Checked = False And CHK4.Checked = False Then
+            m = MessageBox.Show("Please select a source channel!", "No Channel Selected", _
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
 
         If CHK1.Checked = True Then
-            chanNum = 1
-            c += 1
+            Try
+                setChan1()
+            Catch ex As Exception
+                m = MessageBox.Show("Please enter a channel range!", "No Range Entered", _
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                If m = Windows.Forms.DialogResult.OK Then
+                    TXT1.Clear()
+                    TXT1.Focus()
+                    Exit Sub
+                End If
+            End Try
         End If
 
         If CHK2.Checked = True Then
-            chanNum = 2
-            c += 1
+            Try
+                setChan2()
+            Catch ex As Exception
+                m = MessageBox.Show("Please enter a channel range!", "No Range Entered", _
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                If m = Windows.Forms.DialogResult.OK Then
+                    TXT1.Clear()
+                    TXT1.Focus()
+                    Exit Sub
+                End If
+            End Try
         End If
 
         If CHK3.Checked = True Then
-            chanNum = 3
-            c += 1
+            Try
+                setChan3()
+            Catch ex As Exception
+                m = MessageBox.Show("Please enter a channel range!", "No Range Entered", _
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                If m = Windows.Forms.DialogResult.OK Then
+                    TXT1.Clear()
+                    TXT1.Focus()
+                    Exit Sub
+                End If
+            End Try
         End If
 
         If CHK4.Checked = True Then
-            chanNum = 4
-            c += 1
+            Try
+                setChan4()
+            Catch ex As Exception
+                m = MessageBox.Show("Please enter a channel range!", "No Range Entered", _
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                If m = Windows.Forms.DialogResult.OK Then
+                    TXT1.Clear()
+                    TXT1.Focus()
+                    Exit Sub
+                End If
+            End Try
         End If
 
-        If c > 1 Then
-            m = MessageBox.Show("Please select only one channel!", "Error", _
-                                MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            If m = 1 Then
-                TXT1.Clear()
-                TXT1.Focus()
-                CHK1.Checked = False
-                CHK2.Checked = False
-                CHK3.Checked = False
-                CHK4.Checked = False
-            End If
-        End If
+    End Sub
 
-        If c < 1 Then
-            m = MessageBox.Show("Please select a channel", "Error", _
-                                MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            If m = 1 Then
-                TXT1.Clear()
-                TXT1.Focus()
-            End If
-        End If
+    Private Sub BTN6_Click(sender As Object, e As EventArgs) Handles BTN6.Click
 
-        chanRng = TXT1.Text * 8
+        'scopeData.Close()
 
-        chanRange = ":Channel" + chanNum + ":Range " + chanRng.ToString
-
-        myScope.WriteString(chanRange)
+        End
 
     End Sub
 
