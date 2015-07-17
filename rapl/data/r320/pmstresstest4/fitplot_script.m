d29ed970ab284373774ea8b55456fad4cd9a1539
Joe <joseph-hall@uiowa.edu>
2015-07-16 20:45:47 -0500

    edited some data analysis scripts and produced nice plots
    Former-commit-id: c79bcbc2ff14268fb4c12196e34cc7d575ea80f4 [formerly c79bcbc2ff14268fb4c12196e34cc7d575ea80f4 [formerly 3e80c8d567472105cebc03a2ab959ada2259eb96]]
    Former-commit-id: 5a3ca8472abfb32a976f8b0faa3452a44f0089b9
    Former-commit-id: 9acc9b0eeae48d4b5289c24cee331593ddbe7194

--- rapl/data/r320/pmstresstest4/fitplot_script.m
+++ rapl/data/r320/pmstresstest4/fitplot_script.m
@@ -1,6 +1,6 @@
 % fit error
 
-linfit=46.5+0.936.*p_pg_interp;
+linfit=46.49341+0.985322.*p_pg_interp; % from Y=a+b*x of pmstress_4_5_polyfit.xls
 fiterror=prms_ave-linfit;
 fiterror_pcnt=100*fiterror./prms_ave;
 
@@ -40,3 +40,22 @@ set(h15,'marker','s','markersize',5,'linestyle','none');
 set(h13,'marker','o','markersize',5,'linestyle','none');
 
 print -dpdf fiterrorplot.pdf
+
+f7=figure;
+[ax,h17,h19]=plotyy(prms_ave,fiterror,prms_ave,fiterror_pcnt);
+axes(ax(1));
+xlabel('RMS Power (watts)');
+ylabel(ax(1),'Error (watts)');
+ylabel(ax(2),'% Error');
+grid on;
+T7 = title({"Linear Fit Error vs RMS Power"});
+axes(ax(2));
+FS7=findall(gca,'-property','Fontsize');
+set(FS7, "fontsize", 12, "linewidth", 2);
+axes(ax(1));
+FS7=findall(gca,'-property','Fontsize');
+set(FS7, "fontsize", 12, "linewidth", 2);
+set(h19,'marker','s','markersize',5,'linestyle','none');
+set(h17,'marker','o','markersize',5,'linestyle','none');
+
+print -dpdf fiterrorVpowerplot.pdf
