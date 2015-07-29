1e55ef8b7f126b65d13ee26b39843cd4fe2660bc
Josiah McClurg <jcmcclurg@gmail.com>
2015-07-29 12:26:39 -0500

    update

--- powerclamp/powerclamp.sh
+++ powerclamp/powerclamp.sh
@@ -3,7 +3,7 @@
 device=${1:-/sys/class/thermal/cooling_device4}
 
 rangeMax=`cat $device/max_state`
-rangeMin=0
+rangeMin=1
 
 echo Welcome to the powerclamp cpu limiter. Please make sure you have >&2
 echo the module loaded. Type the limiting values. Press CTRL+C or q to exit. >&2
@@ -19,7 +19,7 @@ while [[ "$limitVal" != "q" ]]; do
 	elif [ $(echo "$limitVal > $rangeMax" | bc) == 1 ]; then
 		limitVal=$rangeMax
 	fi
-	echo "$rangeMax - $limitVal" | bc > $device/cur_state
+	echo "1 + $rangeMax - $limitVal" | bc > $device/cur_state
 	echo "Set to $limitVal" >&2
 	read limitVal
 done
