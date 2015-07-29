dfa28b62ecee66beb211de2bf14e57a0dc3a336c
Josiah McClurg <jcmcclurg@gmail.com>
2015-07-29 09:49:50 -0500

    Open loop power for Hadoop Kmeans experiment
    Former-commit-id: b494ebed9052fae0c097c1bf4d6af69bf900a325

--- integralController/closed_loop/no_power_control.sh
+++ integralController/closed_loop/no_power_control.sh
@@ -13,7 +13,8 @@ sudo $powerGadgetCmd -e 500 -c tp > $dir/$me.$d.power 2>> $me.$d.output &
 pgid=$!
 
 echo "Sarting closed loop control at "$(date +%s.%N) | tee -a $me.$d.output >&2
-(sudo $powerGadgetCmd -e 150 & pgid2=$!; ($dir/../../../HiBench/bin/run-all.sh >> $me.$d.output 2>> $me.$d.output; echo "q"; sudo kill -s SIGINT $pgid2) & sleep 5; $dir/playback -f $powerProfile -p s) | ($dir/../integralController -s 34 -n 0 -x 1 -t 0.1 -k 0.005 -d 0 -u 10; echo "q") | $dir/donothing.sh >> $me.$d.output 2>> $me.$d.output
+#(sudo $powerGadgetCmd -e 150 & pgid2=$!; ($dir/../../../HiBench/bin/run-all.sh >> $me.$d.output 2>> $me.$d.output; echo "q"; sudo kill -s SIGINT $pgid2) & sleep 5; $dir/playback -f $powerProfile -p s) | ($dir/../integralController -s 34 -n 0 -x 1 -t 0.1 -k 0.005 -d 0 -u 10; echo "q") | $dir/donothing.sh >> $me.$d.output 2>> $me.$d.output
+(sudo $powerGadgetCmd -e 150 & pgid2=$!; $dir/playback -f $powerProfile -p p & playbackid=$!; $dir/../../../HiBench/bin/run-all.sh >> $me.$d.remote 2>> $me.$d.remote; sudo kill -s SIGINT $pgid2 $playbackid; sleep 1; echo "q") | $dir/donothing.sh >> $me.$d.output 2>> $me.$d.output
 
 head -n 1 $dir/../../../HiBench/report/hibench.report | tee -a $me.$d.output >&2
 tail -n 1 $dir/../../../HiBench/report/hibench.report | tee -a $me.$d.output >&2
---
+++ integralController/closed_loop/no_power_controlPlot.m
@@ -0,0 +1,48 @@
+## Copyright (C) 2015 U-Josiah-PC\Josiah
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {Function File} {@var{retval} =} stressPlot (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: U-Josiah-PC\Josiah <Josiah@Josiah-PC>
+## Created: 2015-07-23
+
+time='1438180108.343760234';
+
+[status,output] = system(['cat no_power_control.' time '.output | grep HadoopKmeans | tail -n 1 | tr -s \  | cut -d \  -f 4,5']);
+[inputSize duration]=strread(output,'%f %f');
+fprintf(1,'Input size: %g GiB, Duration: %g min, Throughput: %g MiB/s\n', inputSize/(power(2,30)), duration/60, inputSize/(duration*power(2,20)))
+
+[status,output] = system(['cat no_power_control.' time '.output | grep Using | cut -d \  -f 2']);
+setpoint = dlmread(output(1:end-1),',');
+
+actual = dlmread(['no_power_control.' time '.power'],' ');
+actual(:,1) = actual(:,1) - actual(1,1) - 5;
+actual(end-4:end,:) = [];
+
+avtime = block_average(actual(:,1),4);
+avactual = block_average(actual(:,2),4);
+setPointRange = setpoint(:,1) < max(actual(:,1));
+
+plot(actual(:,1),actual(:,2),'y',avtime,avactual,'c',setpoint(setPointRange,1),setpoint(setPointRange,2),'r')
+title('Open-loop benchmark power vs setpoint.')
+legend('Actual (0.5s average)','Actual (2s average)','Setpoint')
+xlabel('Time (s)')
+ylabel('Power (W)')
+axis([0, max(actual(:,1)), 4, 40]);
+print('no_power_control.pdf')
