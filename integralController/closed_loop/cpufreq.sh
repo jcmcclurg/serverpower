566e8f2ca28221d2b45ab70e6f68b4ba4ac022e0
Josiah McClurg <jcmcclurg@gmail.com>
2015-07-30 10:55:37 -0500

    Added cpufreq
    Former-commit-id: a5301219a87a0a368451744d5c4da64b6e8e7b84 [formerly 957432c0d34d47ca5bf2ba637669b18b84b44f70]
    Former-commit-id: 26519b41a9a839bb31dc1101ea82e6a36b01d721

---
+++ integralController/closed_loop/cpufreq.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+me=$(basename $0 .sh)
+dir=$(dirname $0)
+powerProfile=$dir/powerDeviation.txt
+powerGadgetCmd=$dir/../../signal_insert_delays/power_gadget_whileloop/power_gadget
+d=$(date +%s.%N)
+
+echo "Writing to $me.$d.output and $me.$d.power" >&2
+echo "Starting power gadget at "$(date +%s.%N) | tee -a $me.$d.output >&2
+echo "Using $powerProfile as power profile." | tee -a $me.$d.output >&2
+
+sudo $powerGadgetCmd -e 500 -c tp > $dir/$me.$d.power 2>> $me.$d.output &
+pgid=$!
+
+echo "Starting closed loop control at "$(date +%s.%N) | tee -a $me.$d.output >&2
+
+# With tee and stress
+(sudo $powerGadgetCmd -e 150 & pgid2=$!; stress -c 4 & playbackid=$!; tee; sudo kill -s SIGINT $pgid2 $playbackid; echo "q") | ($dir/../integralController -s 34 -n 1200000 -x 2500000 -t 130000 -k 0 -d 0 -u 10; sleep 1; echo "q") | sudo $dir/../../cpufreq/cpufreq.sh >> $me.$d.output 2>> $me.$d.output
+
+# With integral controller
+(sudo $powerGadgetCmd -e 150 & pgid2=$!; $dir/playback -f $powerProfile -p s & playbackid=$!; $dir/../../../HiBench/bin/run-all.sh >> $me.$d.remote 2>> $me.$d.remote; sudo kill -s SIGINT $pgid2 $playbackid; echo "q") | ($dir/../integralController -s 34 -n 1200000 -x 2500000 -t 130000 -k 0 -d 0 -u 10; sleep 1; echo "q") | sudo $dir/../../cpufreq/cpufreq.sh >> $me.$d.output 2>> $me.$d.output
+
+head -n 1 $dir/../../../HiBench/report/hibench.report | tee -a $me.$d.output >&2
+tail -n 1 $dir/../../../HiBench/report/hibench.report | tee -a $me.$d.output >&2
+
+echo "Stopping power gadget at "$(date +%s.%N) | tee -a $me.$d.output >&2
+sudo kill -s SIGINT $pgid
