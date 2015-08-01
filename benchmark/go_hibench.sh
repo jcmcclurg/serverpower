71939182d8013872bd1cd6a905fb1525a1b7e887
Josiah McClurg <jcmcclurg@gmail.com>
2015-08-01 11:09:39 -0500

    incremental update on signal_insert_delays
    Former-commit-id: be6fd58cce4e1117e6fef918c49b960846224674 [formerly 09780f3988c0a8d55375bb1a89ada37d70898682]
    Former-commit-id: ebcf3570d64a9a221044432405ebdc5f30951128

--- benchmark/go_hibench.sh
+++ benchmark/go_hibench.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+echo "Stopping YARN and DFS at "`date +%s.%N`
+../../hadoop-2.7.1/sbin/stop-yarn.sh 2>&1
+../../hadoop-2.7.1/sbin/stop-dfs.sh 2>&1
+
 echo "Formatting namenode at "`date +%s.%N`
-../../hadoop-2.7.0/bin/hdfs namenode -format 2>&1
+../../hadoop-2.7.1/bin/hdfs namenode -format 2>&1
 
-echo "Starting DFS at "`date +%s.%N`
-../../hadoop-2.7.0/sbin/start-dfs.sh 2>&1
-
-echo "Starting YARN at "`date +%s.%N`
-../../hadoop-2.7.0/sbin/start-yarn.sh 2>&1
+echo "Starting DFS and YARN at "`date +%s.%N`
+../../hadoop-2.7.1/sbin/start-dfs.sh 2>&1
+../../hadoop-2.7.1/sbin/start-yarn.sh 2>&1
 
 echo "Starting HiBench at "`date +%s.%N`
+free -m
 ../../HiBench/bin/run-all.sh 2>&1
+free -m
