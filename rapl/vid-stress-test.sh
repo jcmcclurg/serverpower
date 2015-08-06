6f8519ab4bf44297982a2bba9081b6f53c4de1b5
Joseph Hall <joseph-hall@uiowa.edu>
2015-08-06 16:34:42 -0500

    created rapl/data/r320/fmdtest1/
    Former-commit-id: 53fa5128111d8ade6894021edb36f95981eac464 [formerly 1fd07467ac4e1f16525645320210eb5d3f2b7887] [formerly 1fd07467ac4e1f16525645320210eb5d3f2b7887 [formerly 550d0c0f60531da004bc0718415fc1fb7423a27e]]
    Former-commit-id: db6afbc1a0e6cc0fcdd8262171fc15b920a18575
    Former-commit-id: ec109f9c1dd161e724121ff87b52615c165ecb3f

--- rapl/vid-stress-test.sh
+++ rapl/vid-stress-test.sh
@@ -6,7 +6,7 @@ avconv -i ~/joe/serverpower/transcoders/videos/cut1.mp4 -r 30 -y ~/joe/serverpow
 avconv -i ~/joe/serverpower/transcoders/videos/cut1.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output3.avi > ~/joe/serverpower/transcoders/videos/log3.csv & pgid3=$!
 avconv -i ~/joe/serverpower/transcoders/videos/cut1.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output4.avi > ~/joe/serverpower/transcoders/videos/log4.csv & pgid4=$!
 
-tee >(stdbuf -i0 -o0 ../xbeecom/RS-232-Lib/fmd2server -r 500 -p 17 -b 115200 -M 35 -m 24 -a 1 -o ../xbeecom/RS-232-Lib/test.csv) | unbuffer -p tee >(./power_gadget -e 500)
+tee >(stdbuf -i0 -o0 ../xbeecom/RS-232-Lib/fmd2server -r 500 -p 17 -b 115200 -M 40 -m 20 -a 1 -o ../xbeecom/RS-232-Lib/test.csv) | unbuffer -p tee >(./power_gadget -e 500)
 
 kill $pgid
 kill $pgid1
