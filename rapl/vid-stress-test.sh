14634037307766311423b41708060be705548910
Joseph Hall <joseph-hall@uiowa.edu>
2015-08-06 15:50:09 -0500

    added vid-stress-test.sh
    Former-commit-id: f8faf172388823acacc829b6dad6e26cb7990fde [formerly f8faf172388823acacc829b6dad6e26cb7990fde [formerly f3b37e8466f07816317762604c2d3786da4b23fa]]
    Former-commit-id: 2e376bb6396ebc5f1e06e1c57de574c3fa6030ce
    Former-commit-id: 5c6cb8120de24387969d379883a22b6665150788

---
+++ rapl/vid-stress-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+avconv -i ~/joe/serverpower/transcoders/videos/cut.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output.avi > ~/joe/serverpower/transcoders/videos/log.csv & pgid=$!
+avconv -i ~/joe/serverpower/transcoders/videos/cut1.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output1.avi > ~/joe/serverpower/transcoders/videos/log1.csv & pgid1=$!
+avconv -i ~/joe/serverpower/transcoders/videos/cut1.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output2.avi > ~/joe/serverpower/transcoders/videos/log2.csv & pgid2=$!
+avconv -i ~/joe/serverpower/transcoders/videos/cut1.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output3.avi > ~/joe/serverpower/transcoders/videos/log3.csv & pgid3=$!
+avconv -i ~/joe/serverpower/transcoders/videos/cut1.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output4.avi > ~/joe/serverpower/transcoders/videos/log4.csv & pgid4=$!
+
+tee >(stdbuf -i0 -o0 ../xbeecom/RS-232-Lib/fmd2server -r 500 -p 17 -b 115200 -M 35 -m 24 -a 1 -o ../xbeecom/RS-232-Lib/test.csv) | unbuffer -p tee >(./power_gadget -e 500)
+
+kill $pgid
+kill $pgid1
+kill $pgid2
+kill $pgid3
+kill $pgid4
