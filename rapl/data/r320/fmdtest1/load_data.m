c70b50e8fe3b4083efcd107c33e34246e6682d71
Joe <joseph-hall@uiowa.edu>
2015-08-10 15:16:24 -0500

    changed plot-* to plot_data.m

--- rapl/data/r320/fmdtest1/load_data.m
+++ rapl/data/r320/fmdtest1/load_data.m
@@ -2,8 +2,8 @@
 
 load data_pg.mat;
 t_pg=data_pg(1:end,1);
-freq_pg=data_pg(1:end,2);
-sp_pg=data_pg(1:end,3);
+pkg_pg=data_pg(1:end,2);
+dram_pg=data_pg(1:end,3);
 
 load data_fmd.mat;
 t_fmd=data_fmd(1:end,1);
--- rapl/data/r320/fmdtest1/plot_data.m
+++ rapl/data/r320/fmdtest1/plot_data.m
@@ -4,7 +4,7 @@ top = min([length(t_log0) length(t_log1) length(t_log2) length(t_log3) length(t_
 fps_ave=(fps_log0(1:top)+fps_log1(1:top)+fps_log2(1:top)+fps_log3(1:top)+fps_log4(1:top))/5;
 
 fh1=figure;
-[ax_pkg,h_pkg]=plot(t_pg,pkg_pg,'or','markersize',10,'displayname','PKG');
+h_pkg=plot(t_pg,pkg_pg,'or','markersize',10,'displayname','PKG');
 hold on;
 [ax_yy,h_sp,h_fps]=plotyy(t_fmd,sp_fmd,t_log0,fps_ave);
 set(h_sp, 'linestyle','--');
