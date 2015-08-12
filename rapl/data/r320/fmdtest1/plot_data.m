1c4c302574820a40aee3748111cbd467e932263a
Joe <joseph-hall@uiowa.edu>
2015-08-12 13:25:52 -0500

    changed plot_data.m
    Former-commit-id: bf22bdf054b8a0830be48f4518c572c883bc6e3b [formerly 4bebf8b83412d4b6a7bac4acb3a53e1e357bd90a] [formerly 4bebf8b83412d4b6a7bac4acb3a53e1e357bd90a [formerly 7708149c21b6232448b1c4fdf254dab849d325f1]]
    Former-commit-id: 65842d2fc55db47c708fc78e91fdff4ecca191a3
    Former-commit-id: dc08174493bec64a9d307a8c4ffa971f70b1dcd9

--- rapl/data/r320/fmdtest1/plot_data.m
+++ rapl/data/r320/fmdtest1/plot_data.m
@@ -5,14 +5,17 @@ fps_ave=(fps_log0(1:top)+fps_log1(1:top)+fps_log2(1:top)+fps_log3(1:top)+fps_log
 
 
 fh1=figure;
-h_pkg=plot(t_pg,pkg_pg,':r','markersize',10,'linewidth',6);
+h_pkg=plot(t_pg,pkg_pg,':r','markersize',10,'linewidth',2);
 hold on;
-[ax_yy,h_sp,h_fps]=plotyy(t_fmd,sp_fmd,t_log0,fps_ave);
-set(h_sp, 'linestyle','--','linewidth',4);
-set([h_pkg h_fps],'linewidth',4);
+[x_sp,y_sp]=stairs(t_fmd,sp_fmd);
+[ax_yy,h_sp,h_fps]=plotyy(x_sp,y_sp,t_log0,fps_ave);
+set(h_sp, 'linestyle','--','linewidth',2);
+set([h_pkg h_fps],'linewidth',2);
 ax_sp=ax_yy(1);
+%ylim(ax_sp,[20,35]);
 ax_fps=ax_yy(2);
-axes(ax_sp);
+%ylim(ax_fps,[50,150]);
+%axes(ax_sp);
 xlabel('Time (seconds)');
 ylabel(ax_sp,'Power (watts)');
 ylabel(ax_fps,'frames per second');
@@ -22,15 +25,17 @@ T1 = title({"Video Transcoding DR with Frequency Deviation as Input"});
 set(hleg_obj,'linewidth',4);
 FS=findall(hleg_obj,'-property','fontsize');
 set(FS,'fontsize',18);
-legend boxoff;
 FS=findall(ax_sp,'-property','Fontsize');
-set(FS, "fontsize", 18, "linewidth", 4);
+set(FS, "fontsize", 18, "linewidth", 2);
 FS=findall(ax_fps,'-property','Fontsize');
-set(FS, "fontsize", 18, "linewidth", 4);
+set(FS, "fontsize", 18, "linewidth", 2);
+legend boxoff;
+hold off;
 
-
-
-%plot(t_pg,pkg_pg,'r');
-%hold on;
+fh2=figure;
+plot(t_fmd,freq_fmd/1000-60,'r');
+hold on;
+[xs,ys]=stairs(t_fmd,freq_fmd/1000-60);
+plot(xs,ys,'b');
 %plotyy(t_fmd,sp_fmd,t_fmd,freq_fmd/1000-60);
 
