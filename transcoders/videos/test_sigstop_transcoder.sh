a4d8b2eba6d8f9bfac97d213132d69fdb7c41730
Joseph Hall <joseph-hall@uiowa.edu>
2015-08-14 16:36:14 -0500

    test_sigstop_transcoder.sh working

--- transcoders/videos/test_sigstop_transcoder.sh
+++ transcoders/videos/test_sigstop_transcoder.sh
@@ -2,7 +2,7 @@
 
 #avconv -i ~/joe/serverpower/transcoders/videos/cut.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output.avi > ~/joe/serverpower/transcoders/videos/log.csv & pgid=$!
 
-avconv -i ~/joe/serverpower/transcoders/videos/cut.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output.avi & pgid=$!
+avconv -i ~/joe/serverpower/transcoders/videos/cut.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output.avi & pid=$!
+#stress -m 2 & pid2=$!
 
-echo $pgid
-#~/joe/serverpower/signal_insert_delays/insertDelays -v -U -u 10 -w 0.3 -o -p $pgid
+~/joe/serverpower/signal_insert_delays/insertDelays -U -d 0.5 -w 0.01 -p $pid #$pid2
