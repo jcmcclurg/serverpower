859bd6d0086dbe06eb12e11d7e7d10f2b8760d11
Joseph Hall <joseph-hall@uiowa.edu>
2015-08-17 11:05:03 -0500

    copied checkusage.sh and stop_x2goserver.sh to serverpoiwer/
    Former-commit-id: 2ccb5396b51e575af180fced3804c4b07f62afdc [formerly 2ccb5396b51e575af180fced3804c4b07f62afdc [formerly 9074e823c3a9c4f420745cd3e541bdf03808d22e]]
    Former-commit-id: ba6d8adf371fb616279e1eaf1d45e48b814f3115
    Former-commit-id: eeb76e2d7908df893fe6b8864d7e3bebc0165289

--- transcoders/videos/test_sigstop_transcoder.sh
+++ transcoders/videos/test_sigstop_transcoder.sh
@@ -4,5 +4,8 @@
 
 avconv -i ~/joe/serverpower/transcoders/videos/cut.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output.avi & pid=$!
 #stress -m 2 & pid2=$!
+#avconv -i ~/joe/serverpower/transcoders/videos/cut1.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output1.avi & pid2=$!
 
 ~/joe/serverpower/signal_insert_delays/insertDelays -U -d 0.5 -w 0.01 -p $pid #$pid2
+
+kill $pid
