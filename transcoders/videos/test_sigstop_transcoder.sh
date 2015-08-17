8c10e48133efa645718842d8c18829816c9d5c28
Joseph Hall <joseph-hall@uiowa.edu>
2015-08-17 14:51:18 -0500

    added ...../transcoders/videos/go_transcoder_FMD_control_test.sh

--- transcoders/videos/test_sigstop_transcoder.sh
+++ transcoders/videos/test_sigstop_transcoder.sh
@@ -9,3 +9,6 @@ avconv -i ~/joe/serverpower/transcoders/videos/cut.mp4 -r 30 -y ~/joe/serverpowe
 ~/joe/serverpower/signal_insert_delays/insertDelays -U -d 0.5 -w 0.01 -p $pid #$pid2
 
 kill $pid
+
+
+
