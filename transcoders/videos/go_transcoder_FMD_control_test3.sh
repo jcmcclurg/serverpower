e29867655a2b2a124b03cb13d6d3b972f516ee6e
Joseph Hall <joseph-hall@uiowa.edu>
2015-09-21 10:39:06 -0500

    added transcoderFMDController.c

--- transcoders/videos/go_transcoder_FMD_control_test3.sh
+++ transcoders/videos/go_transcoder_FMD_control_test3.sh
@@ -13,7 +13,7 @@ insert_delays="$dir/signal_insert_delays/insertDelays"
 
 #rm $transcd_log
 
-(unbuffer -p avconv -i $vid_input -r 30 -y $vid_output 2>/dev/null; echo "S"; sleep 1; echo "q") | (sudo $power_monitor -e 150 & pgid2=$!; sudo $transcd_ctrlr -r 500 -p 16 -b 115200 -M 30 -m 16 -a 1 -B 15000 -o $transcd_log; sudo kill -s SIGINT $pgid2; sleep 1; echo "q") | ($integral_ctrlr -s 28 -n 0 -x 1 -t 0.1 -k 0 -d 0 -u 10; sleep 1; echo "q") | (sleep 1; $insert_delays -U -d 0.5 -p $(pgrep avconv))
+(unbuffer -p avconv -i $vid_input -r 30 -y $vid_output 2>/dev/null; echo "S"; sleep 1; echo "q") | (sudo $power_monitor -e 150 & pgid2=$!; sudo $transcd_ctrlr -r 500 -p 16 -b 115200 -M 30 -m 16 -a 1 -d 1 -B 2000 -o $transcd_log; sudo kill -s SIGINT $pgid2; sleep 1; echo "q") | ($integral_ctrlr -s 28 -n 0 -x 1 -t 0.1 -k 0 -d 0 -u 10; sleep 1; echo "q") | (sleep 1; $insert_delays -U -d 0.5 -p $(pgrep avconv))
 
 #| ~/joe/serverpower/integralController/integralController -s 28 -n 0 -x 1 -t 0.1 -k 0 -d 0 -u 10; sleep 1; echo "q") | ~/joe/serverpower/signal_insert_delays/insert 
 
