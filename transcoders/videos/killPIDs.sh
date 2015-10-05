06504b6d0ce6999cfedae5fb08560d3522190f6f
Joseph Hall <joseph-hall@uiowa.edu>
2015-10-05 14:10:13 -0500

    added t.J.P.2.W.D data

--- transcoders/videos/killPIDs.sh
+++ transcoders/videos/killPIDs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # kill processes saved in go_transcoder_FMD_control_test4.sh
+sudo kill -KILL $(pgrep goNoDeadlines)
+sudo kill -KILL $(pgrep goTranscoders)
 sudo kill -KILL $(pgrep calcSetpoint)
 sudo kill -KILL $(pgrep avconv)
 sudo kill -KILL $(pgrep power_gadget)
 sudo kill -KILL $(pgrep getFreq)
 sudo kill -KILL $(pgrep insertDelays)
 sudo kill -KILL $(pgrep integralController)
+
 
 
