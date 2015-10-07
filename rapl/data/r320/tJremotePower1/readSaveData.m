5389d2ed691d303c0930610c20ed5149a419b706
Joe <joseph-hall@uiowa.edu>
2015-10-07 16:56:23 -0500

    commiting before updating tJremotePower1 data
    Former-commit-id: 8bb95673f880ff2e8c7801a6e39263ae49945fd8 [formerly 8bb95673f880ff2e8c7801a6e39263ae49945fd8 [formerly 315cf06bcfbcb7e20c284814b0273d7413260380]]
    Former-commit-id: f115767cb977ba01b4c867c5584612f13f554484
    Former-commit-id: e0cf58dd31472a8e8af4271851ab83c5df65070c

--- rapl/data/r320/tJremotePower1/readSaveData.m
+++ rapl/data/r320/tJremotePower1/readSaveData.m
@@ -4,14 +4,13 @@ clear all;
 clc;
 
 %open power_gadget data file and get data
-fid = fopen('pg_data.csv');
-pg_data = textscan(fid,'%f %f %f','Delimiter',',','Headerlines',1);
+fid = fopen('remoteData.csv');
+pg_data = textscan(fid,'%f %f','Delimiter',',','Headerlines',0);
 fclose(fid);
 % convert data and save to .mat
 t_pg=pg_data{1};
 pkg_pg=pg_data{2};
-dram_pg=pg_data{3};
-data_pg = [t_pg pkg_pg dram_pg];
+data_pg = [t_pg pkg_pg];
 save data_pg.mat data_pg;
 
 %open fmd output data file and get data
@@ -38,4 +37,26 @@ frame_fmd=fmd_data{4};
 data_fmd2 = [t_fmd freq_fmd sp_fmd frame_fmd];
 save data_fmd2.mat data_fmd2;
 
+%open fmd output data file and get data
+fid = fopen('formatedData3.csv');
+fmd_data = textscan(fid,'%f %f %f %f','Delimiter',',','Headerlines',0);
+fclose(fid);
+% convert data and save to .mat
+t_fmd=fmd_data{1};
+freq_fmd=fmd_data{2};
+sp_fmd=fmd_data{3};
+frame_fmd=fmd_data{4};
+data_fmd3 = [t_fmd freq_fmd sp_fmd frame_fmd];
+save data_fmd3.mat data_fmd3;
 
+%open fmd output data file and get data
+fid = fopen('formatedData4.csv');
+fmd_data = textscan(fid,'%f %f %f %f','Delimiter',',','Headerlines',0);
+fclose(fid);
+% convert data and save to .mat
+t_fmd=fmd_data{1};
+freq_fmd=fmd_data{2};
+sp_fmd=fmd_data{3};
+frame_fmd=fmd_data{4};
+data_fmd4 = [t_fmd freq_fmd sp_fmd frame_fmd];
+save data_fmd4.mat data_fmd4;
