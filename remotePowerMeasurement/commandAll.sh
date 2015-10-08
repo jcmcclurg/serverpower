08747cad29a9401bc98f4d0713474de0c9af140a
Josiah McClurg <jcmcclurg@gmail.com>
2015-10-08 14:12:59 -0700

    added a program to talk to all the servers at once

---
+++ remotePowerMeasurement/commandAll.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ssh josiah@jjpowerserver.ddns.net $@
+ssh josiah@jjpowerserver2.ddns.net -p 10001 $@
+ssh josiah@jjpowerserver2.ddns.net -p 10002 $@
+ssh josiah@jjpowerserver2.ddns.net -p 10003 $@
+ssh josiah@jjpowerserver2.ddns.net -p 10004 $@
