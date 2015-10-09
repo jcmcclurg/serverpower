a036d6640d325f59c18aace58e91860e017e6377
Josiah <jcmcclurg@gmail.com>
2015-10-09 11:52:19 -0500

    Fixed streaming power measurement
    Former-commit-id: a23a21bd0690b70d54a5d32afa49ec1e4b6de759 [formerly 94f35024ded618553023934581195957f9891c0b] [formerly 94f35024ded618553023934581195957f9891c0b [formerly 81c7c4a2f25a1a1b1dfbb262d78c8f8876ba3d0c]]
    Former-commit-id: 96cc919b51270482d6f64910b7edbc32b506dcaf
    Former-commit-id: 617e1311ec745ad1c96175e3c4d148cb97fb1aa5

--- remotePowerMeasurement/streamPowers.sh
+++ remotePowerMeasurement/streamPowers.sh
@@ -4,10 +4,17 @@ addr="jjpowerserver2.ddns.net:18282"
 #defDuration="0.150"
 defDuration="0.500"
 duration=${1:-$defDuration}
-numCycles=$( printf "%.0f" $( bc <<< "scale=1; 60*$duration" ) )
-numSamples=$( printf "%.0f" $( bc <<< "scale=1; $numCycles*10000/60" ) )
+echo $duration
 
-callsPerBlock=$( printf "%0.f" $( bc <<< "scale=1; 30/$duration" ) )
+numCycles=$( bc <<< "scale=1; 60*$duration" | tr -d '[[:space:]]' )
+numCycles=$( printf "%.0f" $numCycles )
+
+numSamples=$( bc <<< "scale=1; $numCycles*10000/60" | tr -d '[[:space:]]' )
+numSamples=$( printf "%.0f" $numSamples )
+
+callsPerBlock=$( bc <<< "scale=1; 30/$duration" | tr -d '[[:space:]]' )
+callsPerBlock=$( printf "%.0f" $callsPerBlock )
+
 
 while true; do
 	(
