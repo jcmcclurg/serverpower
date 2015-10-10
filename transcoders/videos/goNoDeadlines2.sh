3efb295f06c5e965cd63e78dfd601cb6b0956269
Joseph Hall <joseph-hall@uiowa.edu>
2015-10-09 20:17:06 -0500

    added data for tJremotePower2 Kd=0.1
    Former-commit-id: d53cb1cbb33897d8ca549bf44ebf9cd2c82490a3 [formerly 7d321f65a3aeda029fb06603543cb43fc84ce7f4] [formerly 7d321f65a3aeda029fb06603543cb43fc84ce7f4 [formerly 2e9e6d571b01d47031bccc9e69f6e3190fe990ca]]
    Former-commit-id: a1c07a630ead56a0ac469bca6f77f4ea07d06fba
    Former-commit-id: ece69770cc02416f0a71a66f902bfe5798d400ac

--- transcoders/videos/goNoDeadlines2.sh
+++ transcoders/videos/goNoDeadlines2.sh
@@ -42,7 +42,7 @@ while true; do
 	avconv -i $vIn/cut3.mp4 -r 30 -y $vOut/out3.avi > frames4 & pid=$!
 	#setpoint = $(echo "setpoint$pid")
 	(tail -f -q --pid=$pid freq frames4; echo "q") | $calcSetpoint -d 0 -M $maxPower -m $minPower -o $logPath/calcSet4Data.csv > setpoint4 &
-	(tail -f -q --pid=$pid setpoint4 power; echo "q") | $integralController -s $minPower -n 0 -x 1 -t 0.1 -k 0 -d 0 -u 10 | $insertDelays -U -d 0.5 -p $pid &
+	(tail -f -q --pid=$pid setpoint4 power; echo "q") | $integralController -s $minPower -n 0 -x 1 -t 0.1 -k 0 -d 0.1 -u 10 | $insertDelays -U -d 0.5 -p $pid &
 	wait $pid
 	#rm $setpoint
 done
