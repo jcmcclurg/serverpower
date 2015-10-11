148dde34f606da8d007b62ce5e6a30bb112425e8
Josiah <jcmcclurg@gmail.com>
2015-10-11 05:35:49 -0500

    Updated power measurement server to include frequency estimation
    Former-commit-id: b3ec0260761273a4c192fd6a6945144fc5e73d35 [formerly 66efe75a8e9648ea7139226864d1631b255ab39f]
    Former-commit-id: 1bd4dcad398196ee7d2c1b67c0c28cb498acf5cb

--- remotePowerMeasurement/measurementServer/measurementServer.py
+++ remotePowerMeasurement/measurementServer/measurementServer.py
@@ -16,9 +16,12 @@ import win32api
 import datetime
 import cgi
 
+from sineFit import *
+
 class MeasurementServer(object):
-	def __init__(self, task):
+	def __init__(self, task, port=8282):
 		self.task = task
+		self.port = port
 
 	def webApp(self, environ, start_response):
 		if environ['PATH_INFO'] == '/csv':
@@ -62,7 +65,7 @@ class MeasurementServer(object):
 		print "Started."
 		try:
 			win32api.SetConsoleCtrlHandler(self.handler)
-			waitress.serve(self.webApp,host='0.0.0.0', port=8282)
+			waitress.serve(self.webApp,host='0.0.0.0', port=self.port)
 		except:
 			pass
 		finally:
@@ -88,7 +91,7 @@ if __name__ == '__main__':
 	sys.stdout = flushfile(sys.stdout)
 
 	class MyMeasurementServer(MeasurementServer):
-		def __init__(self):
+		def __init__(self, port=None):
 			self.voltageScalingFactor = 124.0/6.55;
 
 			rackResistor = 0.02;
@@ -102,6 +105,7 @@ if __name__ == '__main__':
 			self.voltageIndex = 1;
 			self.rackIndex = 2;
 			self.serverIndices = np.array([0, 4, 3, 2, 1]) + 3;
+			self.sampleRate = 10000;
 
 			channels = []
 			channels.append(AIChannelSpec('JDAQ', 0, 'voltage', termConf=DAQmx_Val_Diff, rangemin=-10, rangemax=10))
@@ -112,13 +116,24 @@ if __name__ == '__main__':
 			channels.append(AIChannelSpec('JDAQ', 5, 'server2', termConf=DAQmx_Val_Diff, rangemin=-5, rangemax=5))
 			channels.append(AIChannelSpec('JDAQ', 6, 'server1', termConf=DAQmx_Val_Diff, rangemin=-5, rangemax=5))
 
-			m = MultiChannelAITask(channels,sampleRate=10000)
-			super(MyMeasurementServer, self).__init__(m)
+			m = MultiChannelAITask(channels,sampleRate=self.sampleRate,dataWindowLength=int(np.round(self.sampleRate*100.0)))
+			if port is None:
+				super(MyMeasurementServer, self).__init__(m)
+			else:
+				super(MyMeasurementServer, self).__init__(m, port)
 
 		def webApp(self, environ, start_response):
 			#print "%s serving %s to %s"%(datetime.datetime.now().isoformat(), environ['PATH_INFO'], environ['REMOTE_ADDR'])
-			if environ['PATH_INFO'] in ['/csvScaled', '/power']:
-				defaultLen = 10;
+			if environ['PATH_INFO'] in ['/csvScaled', '/power', '/freq']:
+				contentType = None
+
+				if environ['PATH_INFO'] == '/csvScaled':
+					defaultLen = 100
+				elif environ['PATH_INFO'] == '/power':
+					defaultLen = 1000
+				elif environ['PATH_INFO'] == '/freq':
+					defaultLen = 10000
+
 				qs = cgi.parse_qs(environ['QUERY_STRING'])
 
 				length = int(cgi.escape(qs.get('l', [str(defaultLen)])[0]));
@@ -133,15 +148,17 @@ if __name__ == '__main__':
 				b[:,self.serverIndices] *= self.serverScalingFactor;
 				b[:,3:8] = b[:,self.serverIndices]
 
+				numSamples = b.shape[0]
+
 				if environ['PATH_INFO'] == '/csvScaled':
 					s = StringIO.StringIO()
 					np.savetxt(s,b)
 					output = s.getvalue()
-				else:
-					defaultBlockLen = length
+				elif environ['PATH_INFO'] == '/power':
+					defaultBlockLen = numSamples
 					blockLen = int(cgi.escape(qs.get('b', [str(defaultBlockLen)])[0]))
-					blockLen = np.max([1 , np.min([blockLen, length])])
-					numBlocks = int(np.ceil(float(length)/float(blockLen)))
+					blockLen = np.max([1 , np.min([blockLen, numSamples])])
+					numBlocks = int(np.ceil(float(numSamples)/float(blockLen)))
 
 					b[:,self.rackIndex] *= b[:,self.voltageIndex];
 					b[:,self.serverIndices[0]] *= b[:,self.voltageIndex];
@@ -154,13 +171,14 @@ if __name__ == '__main__':
 					offset = 0
 					for i in range(0,numBlocks-1):
 						blockRange = np.arange(0,blockLen) + offset
-						r[i,2:] = np.mean(b[blockRange,2:],axis=0)
 						r[i,0] = b[blockRange[0],0]
 						r[i,1] = b[blockRange[-1],0]
+						r[i,2:] = np.mean(b[blockRange,2:],axis=0)
 						offset += blockLen
-					r[-1,2:] = np.mean(b[offset:,2:],axis=0)
+
 					r[-1,0] = b[offset,0]
 					r[-1,1] = b[-1,0]
+					r[-1,2:] = np.mean(b[offset:,2:],axis=0)
 
 					s = StringIO.StringIO()
 					np.savetxt(s,r)
@@ -169,11 +187,146 @@ if __name__ == '__main__':
 					#np.savetxt(s2,b)
 					#output = s.getvalue()+"\n\n"+s2.getvalue()
 					output = s.getvalue()
+				elif environ['PATH_INFO'] == '/freq':
+					defaultEstimatorType = "fft"
+					estimatorType = str(cgi.escape(qs.get('t', [str(defaultEstimatorType)])[0]))
 
+					if estimatorType in ['nlls', 'fft'] and length >= 10:
+						nominalAmplitude = 120.0*np.sqrt(2.0)
+						nominalFreq = 60.0
+
+						sampleRate = self.task.sampleRate
+
+						if estimatorType == 'nlls':
+							defaultBlockLen = int(np.round(10.0*sampleRate/nominalFreq))
+							blockLen = int(cgi.escape(qs.get('b', [str(defaultBlockLen)])[0]))
+							blockLen = np.max([int(np.round(0.5*sampleRate/nominalFreq)) , np.min([blockLen, numSamples])])
+						elif estimatorType == 'fft':
+							defaultBlockLen = length
+							blockLen = int(cgi.escape(qs.get('b', [str(defaultBlockLen)])[0]))
+							blockLen = np.max([10 , np.min([blockLen, numSamples])])
+
+						numBlocks = int(np.ceil(numSamples/float(blockLen)))
+						offset = 0
+
+						eamplitude = nominalAmplitude
+						efreq = nominalFreq
+						ephase = 0.0
+
+						r = np.zeros((numBlocks,4))
+						for i in range(0,numBlocks - 1):
+							blockRange = np.arange(0,blockLen) + offset
+							r[i,0] = b[blockRange[0],0]
+							r[i,1] = b[blockRange[-1],0]
+
+							if estimatorType == 'nlls':
+								eamplitude, efreq, ephase = sineFit(b[blockRange,0],b[blockRange,self.voltageIndex],eamplitude,efreq,ephase)
+								r[i,2] = eamplitude
+								r[i,3] = efreq
+							elif estimatorType == 'fft':
+								# See http://www.ingelec.uns.edu.ar/pds2803/Materiales/Articulos/AnalisisFrecuencial/04205098.pdf
+								X = np.fft.rfft(b[blockRange,self.voltageIndex])
+								aX = np.abs(X)
+								k = np.argmax(aX)
+								delta = -np.real((X[k+1] - X[k-1])/(2*X[k] - X[k-1] - X[k+1]))
+								kpeak = k + delta
+								ftone = kpeak*float(sampleRate)/blockLen
+								r[i,2] = 2*aX[k]/float(blockLen)
+								r[i,3] = ftone
+
+							#r[i,4] = ephase
+							offset += blockLen
+
+						r[-1,0] = b[offset,0]
+						r[-1,1] = b[-1,0]
+						if estimatorType == 'nlls':
+							eamplitude, efreq, ephase = sineFit(b[offset:,0],b[offset:,self.voltageIndex],eamplitude,efreq,ephase)
+							r[-1,2] = eamplitude
+							r[-1,3] = efreq
+						elif estimatorType == 'fft':
+							n = numSamples - offset
+							blockRange = np.arange(0,n) + offset
+							X = np.fft.rfft(b[blockRange,self.voltageIndex])
+							aX = np.abs(X)
+							k = np.argmax(aX)
+							delta = -np.real((X[k+1] - X[k-1])/(2*X[k] - X[k-1] - X[k+1]))
+							kpeak = k + delta
+							ftone = kpeak*float(sampleRate)/n
+							r[-1,2] = 2*aX[k]/float(n)
+							r[-1,3] = ftone
+						#r[-1,4] = ephase
+
+						htmlDisplay = bool(cgi.escape(qs.get('h', [str(0)])[0]))
+						if htmlDisplay:
+							contentType = 'text/html'
+							output="""<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>title</title>
+  </head>
+  <body>
+  <table>
+"""
+							for i in range(0,numBlocks):
+								output += "  <tr>"
+								for j in range(0,3):
+									output += "<td>%f</td>"%(r[i,j])
+
+								if r[i,3] <= 59.980:
+									bgcolor='0000ff'
+									color  ='ffffff'
+								elif r[i,3] <= 59.986:
+									bgcolor='0055ff'
+									color  ='ffffff'
+								elif r[i,3] <= 59.990:
+									bgcolor='00aaff'
+									color  ='ffffff'
+								elif r[i,3] <= 59.994:
+									bgcolor='00ffff'
+									color  ='000000'
+								elif r[i,3] <= 59.998:
+									bgcolor='00ff7f'
+									color  ='000000'
+								elif r[i,3] <= 60.002:
+									bgcolor='00ff00'
+									color  ='000000'
+								elif r[i,3] <= 60.006:
+									bgcolor='7fff00'
+									color  ='000000'
+								elif r[i,3] <= 60.010:
+									bgcolor='ffff00'
+									color  ='000000'
+								elif r[i,3] <= 60.014:
+									bgcolor='ffaa00'
+									color  ='000000'
+								elif r[i,3] <= 60.020:
+									bgcolor='ff5500'
+									color  ='ffffff'
+								else:
+									bgcolor='ff0000'
+									color  ='ffffff'
+
+								output += "<td style='background-color:#%s;color:#%s'>%0.4f</td></tr>\n"%(bgcolor,color, r[i,3])
+
+							output += """
+  </table>
+  </body>
+</html>"""
+						else:
+							s = StringIO.StringIO()
+							np.savetxt(s,r)
+							output = s.getvalue()
+					else:
+						output = "Valid estimator types are 'fft' or 'nlls' and length must be >= 10."
+
+				if contentType is None:
+					contentType = 'text/plain'
 				response_headers = [('Cache-Control', 'no-cache, no-store, must-revalidate'),
 								('Pragma', 'no-cache'),
 								('Expires', '0'),
-								('Content-type', 'text/plain'),
+								('Content-type', contentType),
 								('Content-Length', str(len(output)))]
 				start_response(status, response_headers)
 				return [output]
