ae83dbc6c081c64846d5f07784cc1d2d2292fc30
Josiah <jcmcclurg@gmail.com>
2015-10-11 06:09:08 -0500

    Updated remote power plot to include the whole cluster power measurement
    Former-commit-id: 859f7daeb6d75213217aa367a06626b967e88755

--- remotePowerMeasurement/plotPowers.sh
+++ remotePowerMeasurement/plotPowers.sh
@@ -2,4 +2,6 @@
 
 dir=$(dirname $0)
 
-$dir/streamPowers.sh | stdbuf -oL -eL cut -d \  -f 4,5,6,7,8 | $dir/feedgnuplot/bin/feedgnuplot --line --legend 0 'jjpowerserver' --legend 1 'jjpowerserver1' --legend 2 'jjpowerserver2' --legend 3 'jjpowerserver3' --legend 4 'jjpowerserver4' --stream 0.1 --xlen 120
+cmd="$dir/feedgnuplot/bin/feedgnuplot --line --stream 0.1 --xlen 120"
+
+$dir/streamPowers.sh | tee >(stdbuf -oL -eL cut -d \  -f 3 | $cmd --legend 0 'whole cluster') | stdbuf -oL -eL cut -d \  -f 4,5,6,7,8 | $cmd --legend 0 'jjpowerserver' --legend 1 'jjpowerserver1' --legend 2 'jjpowerserver2' --legend 3 'jjpowerserver3' --legend 4 'jjpowerserver4'
