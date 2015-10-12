8c1d796c9fd20ef103088d2e0bdda337285114a9
Josiah McClurg <jcmcclurg@gmail.com>
2015-10-11 23:02:16 -0500

    Updated plot powers to just be a single window again
    Former-commit-id: 60a46dd69fe6d4350ca30435106261bda4de514e [formerly 8b209a387776a3c0700d1b0c4655d7ea0c40095b] [formerly 8b209a387776a3c0700d1b0c4655d7ea0c40095b [formerly d519fd9247996b43b99bd0405ab4ce312c623354]]
    Former-commit-id: 1e0ae8e9832733fc4676219471a7a1b952b4974f
    Former-commit-id: 14e43309ec6acc69d0024bb96264c1356c40770f

--- remotePowerMeasurement/plotPowers.sh
+++ remotePowerMeasurement/plotPowers.sh
@@ -4,4 +4,8 @@ dir=$(dirname $0)
 
 cmd="$dir/feedgnuplot/bin/feedgnuplot --line --stream 0.1 --xlen 120"
 
-$dir/streamPowers.sh | tee >(stdbuf -oL -eL cut -d \  -f 3 | $cmd --legend 0 'whole cluster') | stdbuf -oL -eL cut -d \  -f 4,5,6,7,8 | $cmd --legend 0 'jjpowerserver' --legend 1 'jjpowerserver1' --legend 2 'jjpowerserver2' --legend 3 'jjpowerserver3' --legend 4 'jjpowerserver4'
+# Two windows: One for the cluster power and one for the individual powers
+#$dir/streamPowers.sh | tee >(stdbuf -oL -eL cut -d \  -f 3 | $cmd --legend 0 'whole cluster') | stdbuf -oL -eL cut -d \  -f 4,5,6,7,8 | $cmd --legend 0 'jjpowerserver' --legend 1 'jjpowerserver1' --legend 2 'jjpowerserver2' --legend 3 'jjpowerserver3' --legend 4 'jjpowerserver4'
+
+# One window for the invidiual powers
+$dir/streamPowers.sh | stdbuf -oL -eL cut -d \  -f 4,5,6,7,8 | $cmd --legend 0 'jjpowerserver' --legend 1 'jjpowerserver1' --legend 2 'jjpowerserver2' --legend 3 'jjpowerserver3' --legend 4 'jjpowerserver4'
