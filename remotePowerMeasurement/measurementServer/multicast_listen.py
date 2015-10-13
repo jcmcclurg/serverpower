5eb271933ec07208acb5592342256a5bb36be1c8
Josiah <jcmcclurg@gmail.com>
2015-10-13 03:00:54 -0500

    Added streaming feature to measurement server
    Former-commit-id: 5c068ebb49004131e714926e1d002dacb2fa121f [formerly 5c068ebb49004131e714926e1d002dacb2fa121f [formerly 8bcabf95dbb4ff07b3db3861c3f2cd79022640e4]]
    Former-commit-id: fd09890582d38aca5660ed61e4b36491cd7a67a7
    Former-commit-id: cede8a0b95cac42f646f8505fdb5c579b427835b

---
+++ remotePowerMeasurement/measurementServer/multicast_listen.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python -u
+"""
+Created on Mon Oct 12 17:18:13 2015
+
+@author: Josiah
+"""
+
+import sys
+import argparse
+from MulticastSocket import *
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Listens to a multicast address and prints the packet data to standard output.')
+	parser.add_argument('-p', '--port', type=int, help='the port on which to listen', default=9999)
+	parser.add_argument('-a', '--address', help='the address on which to listen', default='224.1.1.1')
+	parser.add_argument('-n', '--nonewline', help='do not print a newline after the packet data', action='store_true')
+	parser.add_argument('-s', '--size', type=int, help='maximum size of packets', default=1024)
+	args = parser.parse_args()
+
+	debug=0
+	multicast_endpoint = Endpoint(port=args.port,hostname=args.address)
+	sys.stderr.write("Setting up listener (%s:%d)\n"%(multicast_endpoint.hostname,multicast_endpoint.port))
+	sys.stderr.write("Options are nonewline=%s size=%d\n"%(args.nonewline,args.size))
+	s = MulticastSocket(multicast_endpoint,bind_single=False,debug=debug)
+	running = True
+	while running:
+		try:
+			p = s.readPacket(args.size)
+			if args.nonewline:
+				sys.stdout.write(p.data)
+			else:
+				sys.stdout.write(p.data+"\n")
+		except (KeyboardInterrupt, ValueError) as e:
+			running = False
+	sys.stderr.write("Goodbye.\n")
