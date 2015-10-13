02d87cb4e0e80a5ec7fee496fee02e468bd98964
powerserver <powerserver@jjpowerserver1.jjcluster.net>
2015-10-13 08:19:15 -0500

    asdf
    Former-commit-id: 1124a7af74c257ca76607c345099efeb1b8756de [formerly 77140072cc23065186ce4c94c690e7863b6624b3]
    Former-commit-id: 0af5dddfe158c16bbfd06f1d6c960b110d1274a0

--- transcoders/videos/test_sigstop_transcoder.sh
+++ transcoders/videos/test_sigstop_transcoder.sh
@@ -3,6 +3,6 @@
 #avconv -i ~/joe/serverpower/transcoders/videos/cut.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output.avi > ~/joe/serverpower/transcoders/videos/log.csv & pgid=$!
 
 avconv -i ~/joe/serverpower/transcoders/videos/cut.mp4 -r 30 -y ~/joe/serverpower/transcoders/videos/output.avi & pid=$!
-#stress -m 2 & pid2=$!
+stress -m 2 & pid2=$!
 
-~/joe/serverpower/signal_insert_delays/insertDelays -U -d 0.5 -w 0.01 -p $pid #$pid2
+~/joe/serverpower/signal_insert_delays/insertDelays -U -d 0.5 -w 0.01 -p $pid $pid2
