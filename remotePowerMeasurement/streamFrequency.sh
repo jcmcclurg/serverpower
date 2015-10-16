cbc8c9db011c55e48c457df8af5348817d0d0249
powerserver <powerserver@jjpowerserver1.jjcluster.net>
2015-10-16 18:43:50 -0500

    have most things working on server1
    Former-commit-id: 1c24eebc96786fc24ccd8173afcfcfadc18df2c5

--- remotePowerMeasurement/streamFrequency.sh
+++ remotePowerMeasurement/streamFrequency.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-python -u multicast_listen.py -n -p 9998 -a 224.1.1.2
+dir="/home/powerserver/joe/serverpower/remotePowerMeasurement"
+python -u $dir/measurementServer/multicast_listen.py -n -p 9998 -a 224.1.1.2
