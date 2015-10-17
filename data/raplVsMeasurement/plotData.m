fa566c7e21248ddd0dfa99c29fd81c6b9de60da4
Joe <joseph-hall@uiowa.edu>
2015-10-17 14:38:38 -0500

    forced to commit bad pg_data copy before pull
    Former-commit-id: 2bbe057352480af8fdc4e073684f1fe7e04fba69 [formerly 83cc61127baa5e7fc77ad38099d7e43a4a34e93b]
    Former-commit-id: 68e1e0c496cb79a6b90e500d9c5af84bb2c69ace

--- data/raplVsMeasurement/plotData.m
+++ data/raplVsMeasurement/plotData.m
@@ -3,9 +3,9 @@ clear all; clc; close all;
 source("~/Documents/research/serverpower/data/raplVsMeasurement/loadData.m");
 
 figure
-plot(t_pg,pkg_pg,'->r','linewidth',2,'displayname','RAPL PKG'); hold on
-plot(t_pg,dram_pg+pkg_pg,'-<b','linewidth',2,'displayname','RAPL PKG+DRAM');
-plot(t_rm,p_rm,'-^k','linewidth',2,'displayname','DAQ');
+plot(t_pg,pkg_pg,'->r','linewidth',1,'displayname','RAPL PKG'); hold on
+plot(t_pg,dram_pg+pkg_pg,'-<b','linewidth',1,'displayname','RAPL PKG+DRAM');
+plot(t_rm,p_rm,'-^k','linewidth',1,'displayname','DAQ');
 ylabel('Power [W]');
 xlabel('Time [sec]');
 legend
