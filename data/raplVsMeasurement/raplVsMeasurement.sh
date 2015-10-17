f9eb6bde3d5f5f9acdc917ff61402c10b7b11008
powerserver <powerserver@jjpowerserver1.jjcluster.net>
2015-10-17 15:32:57 -0500

    DutyStep=0.001, Tw=10ms, stress -m 10, 100Sa/step

--- data/raplVsMeasurement/raplVsMeasurement.sh
+++ data/raplVsMeasurement/raplVsMeasurement.sh
@@ -22,7 +22,7 @@ sleepTime=$(bc <<< "scale=3; 10/$iterations")
 while [  $COUNTER -lt $iterations ]; do
 	bc <<< "scale=3; $COUNTER/$iterations"
 	let COUNTER=COUNTER+1 
-	sleep 0.1
+	sleep 1
 done | $insertDelays -U -d 0.5 -w 0.01 -p $sid
 echo "loop finished"
 kill -KILL $(pgrep stress)
