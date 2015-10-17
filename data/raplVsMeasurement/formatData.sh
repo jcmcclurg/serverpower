6edf7e6716c857879fb110a105e2871eaacdc8a9
powerserver <powerserver@jjpowerserver1.jjcluster.net>
2015-10-17 11:57:56 -0500

    fixed error in data/raplVsMeasurement/formatData.sh
    Former-commit-id: 83bea7c901893bc90d739829511cd5f2e8a0f9d2

--- data/raplVsMeasurement/formatData.sh
+++ data/raplVsMeasurement/formatData.sh
@@ -8,9 +8,9 @@ paste -d ',' time_pg.csv pkg_pg.csv dram_pg.csv > pg_data.csv # final output fil
 rm time_pg.csv pkg_pg.csv dram_pg.csv # clean up
 
 # get powerMeasurement output data
-awk -F [' '] '{printf("%.10f,%.3f,\n",$2,$5)}' powerMeasure.csv > powerLogDecimated.csv
+awk -F [,] '{printf("%.10f,%.3f,\n",$2,$5)}' powerMeasure.csv > powerLogDecimated.csv
 awk -F [,] '{system("date -d @"$1" +%T:%N")}' powerLogDecimated.csv > powerFormated.csv
 awk -F [:::,] '{printf("%.16G\n",($1*3600+$2*60+$3+$4/1000000000))}' powerFormated.csv > timeRemote.csv #> /dev/null
 cut -d, -f2 powerLogDecimated.csv > powerRemote.csv
 paste -d ',' timeRemote.csv powerRemote.csv > remoteData.csv # final output file
-rm powerLogDecimated.csv powerFormated.csv timeRemote.csv powerRemote.csv  # clean up
+rm powerLogDecimated.csv powerFormated.csv timeRemote.csv powerRemote.csv formatData.sh~  # clean up
