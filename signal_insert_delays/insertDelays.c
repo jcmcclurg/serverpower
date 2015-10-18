8891ff8e7a0b3195673956ad8b104840c685ee96
powerserver <powerserver@jjpowerserver1.jjcluster.net>
2015-10-17 22:48:53 -0500

    slen needs to be at least 100*Tw in insertDelays.c
    Former-commit-id: 3b10fba2c39849ca209b95f965f88d1e343d41d3 [formerly 98cf360a512de64dee70cceeeb3efdd7f0b44ac1]
    Former-commit-id: d273effa726abc0b2697f81ba6e52b33e6c5f4ac

--- signal_insert_delays/insertDelays.c
+++ signal_insert_delays/insertDelays.c
@@ -421,7 +421,7 @@ int main(int argc, char *argv[]) {
 			}
 			//slen = (1.0-duty)*(currentTime_sec - prevTime_sec);
 			//slen = (1.0-duty)*0.1;
-			slen = (1.0-duty)*work_interval;
+			slen = (1.0-duty)*work_interval*1.0e2;
 			sleeplen = (useconds_t)(slen*1.0e6);
 			//if(verbose)
 			//	fprintf(stderr,"Sleeping for %g seconds\n",slen);
