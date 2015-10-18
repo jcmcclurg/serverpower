7bafea44b1af04bb5c1ef41e709fb731bc41d2c7
powerserver <powerserver@jjpowerserver1.jjcluster.net>
2015-10-18 17:21:59 -0500

    added looptest.sh and data files for 1server control loop test
    Former-commit-id: dd09529d809e2c78bd2939065160413974953828 [formerly 2b93c9fe93cefe00623ed551e5b4871f274c6c56] [formerly 2b93c9fe93cefe00623ed551e5b4871f274c6c56 [formerly f42190b6fae8be4b0ea6b40d4f288020b09cc520]]
    Former-commit-id: 43ec7c7c0000ddbad02e8d8f17d70e345d8c909c
    Former-commit-id: d59ba14f0bd1b817339351844ea9bd9f3c75baa3

---
+++ data/loop1server/looptest.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+dir="/home/powerserver/joe/serverpower"
+powerGadget="$dir/rapl/power_gadget"
+initializePowerMeasure="$dir/remotePowerMeasurement/initializeStreaming.sh"
+powerMeasure="$dir/remotePowerMeasurement/streamPowerFast.sh"
+stopPowerMeasure="$dir/remotePowerMeasurement/stopStreaming.sh"
+insertDelays="$dir/signal_insert_delays/insertDelays"
+getFreq="$dir/remotePowerMeasurement/streamFrequency.sh"
+integralController="$dir/integralController/integralController"
+calcSetpoint="$dir/xbeecom/RS-232-Lib/calcSetpoint"
+
+maxPower=$1
+minPower=$2
+if [[ -n "$maxPower" ]]; then
+	:
+else
+	maxPower=34
+fi
+if [[ -n "$minPower" ]]; then
+	:
+else
+	minPower=18
+fi
+
+mkfifo pipe pipe2
+rm powerMeasure.csv pg_data.csv remoteData.csv calcSet1Data.csv
+
+cat < pipe | $powerGadget -e 100 > /dev/null & pgid=$! 
+
+$initializePowerMeasure
+$powerMeasure | tee powerMeasure.csv | unbuffer -p cut -d , -f 5 > pipe2 & pmid=$! 
+
+$getFreq | $calcSetpoint -d 0 -M $maxPower -m $minPower -o calcSet1Data.csv > pipe2 & 
+
+stress -m 10 & sid=$!
+
+cat < pipe2 | ($integralController -s $maxPower -n 0 -x 1 -t 0.1 -k 0 -d 0 -u 10; sleep 1; echo "q") | (sleep 1; $insertDelays -U -d 0.5 -w 0.001 -p $sid) &
+
+echo "entering sleep"
+sleep 30
+echo "test finished"
+kill -KILL $(pgrep stress)
+
+$stopPowerMeasure
+echo "q" > pipe
+rm pipe pipe2
+kill -KILL $(pgrep power)
+kill -KILL $(pgrep Freq)
+kill -KILL $(pgrep integral)
+kill -KILL $(pgrep insert)
+kill -KILL $(pgrep calc)
+
+./formatData.sh
+
