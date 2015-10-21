90ce9069885ae0d37f4397d1d5e36c1d0f034b97
Josiah <jcmcclurg@gmail.com>
2015-10-21 14:56:54 -0500

    Updated FreqServer to be a class, so we can include it in our PowerMeasurementServer
    Former-commit-id: 7f5d966ac39027fcedb2f177b59e30ea055e9353 [formerly 90a3ce3f40d2441bf9b6eba47d72063848a4e12e]
    Former-commit-id: 8168f44de1c6e070668cf600d0156397067ea5c8

--- remotePowerMeasurement/measurementServer/FreqServer.py
+++ remotePowerMeasurement/measurementServer/FreqServer.py
@@ -1,48 +1,124 @@
 import serial
 import sys
 import argparse
+import time
+import threading
 from MulticastSocket import *
 
-def writeCommand(ser, cmd):
-	ser.write(cmd)
-	return ser.readline()
+class FreqServer(object):
+	def __init__(self, port=9998, address='224.1.1.2', ms_period=500, com='COM3', baud=115200, verbose=False, timeout=10):
+		self.verbose = verbose
+		self.ms_period = ms_period
+		self.running = False
+		self.lock = threading.Lock()
+
+		multicast_endpoint = Endpoint(port=port,hostname=address)
+		self.log("Setting up multicast socket %s:%d...\n"%(multicast_endpoint.hostname,multicast_endpoint.port))
+
+		self.socket = MulticastSocket(multicast_endpoint,bind_single=True,debug=verbose)
+		self.log("Set up multicast socket %s\n"%(self.socket))
+		self.timeout = timeout
+		self.interruptCheckPeriod = 1
+		self.serial = serial.Serial(port=com,baudrate=baud, timeout=self.timeout)
+		self.log("Set up serial port %s\n"%(self.serial))
+		self.updatePeriod(ms_period)
+		self.workerThread = None
+	
+	def serve(self):
+		while self.running:
+			try:
+				ret = self.serial.readline()
+				if (not (ret is None)) and (ret != ""):
+					self.log("Read (%s)\n"%(ret))
+					p = Packet("f"+ret,self.socket.multicast_endpoint)
+					self.socket.sendPacket(p)
+				else:
+					self.log("Read timed out!\n")
+			except:
+				self.log("Error %s\n"%(sys.exc_info()[0]))
+				self.stop()
+		self.log("Thread finished\n")
+
+	def join(self):
+		while self.running:
+			try:
+				self.log("Checking for interrupts...\n")
+				self.workerThread.join(self.interruptCheckPeriod)
+			except KeyboardInterrupt:
+				self.log("Got interrupt.\n")
+				self.running = False
+		self.log("Waiting...\n")
+		self.workerThread.join()
+
+	def stop(self):
+		assert(self.running == True)
+		self.running = False
+		self.join()
+
+		self.lock.acquire()
+		self.writeCommand('S')
+		self.workerThread = None
+		self.lock.release()
+	
+	def start(self, autojoin=True):
+		assert(self.running == False)
+		self.running = True
+
+		self.lock.acquire()
+		self.writeCommand('B')
+		self.workerThread = threading.Thread(target=self.serve)
+		self.workerThread.start()
+		self.lock.release()
+
+		if autojoin:
+			self.join()
+
+	def updatePeriod(self, ms_period, autojoin=True):
+		if self.running:
+			needsRestart = True
+			self.stop()
+		else:
+			needsRestart = False
+
+		self.log("Resetting device to period of %d ms.\n"%(ms_period))
+		self.lock.acquire()
+
+		self.writeCommand('R')
+		bt = [(ms_period >> 8) & 0x00FF, (ms_period) & 0x00FF ]
+		self.writeCommand(bt)
+		ret = self.serial.readline()
+		self.ms_period = ms_period
+		self.lock.release()
+
+		if needsRestart:
+			self.log("Restarting!\n")
+			self.start(autojoin)
+	
+	def writeCommand(self, cmd):
+		self.serial.write(cmd)
+		return self.serial.readline()
+
+	def close(self):
+		self.serial.close()
+		self.socket.close()
+		self.log("Closed.\n")
+
+	def log(self,string):
+		if self.verbose:
+			sys.stderr.write(string)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Reads from standard input and sends to a multicast address.')
 	parser.add_argument('-p', '--port', type=int,	help='the port on which to listen', default=9998)
 	parser.add_argument('-a', '--address', type=str, help='the address on which to listen', default='224.1.1.2')
 	parser.add_argument('-m', '--ms_period', type=int, help='period of reading', default=500)
+	parser.add_argument('-v', '--verbose', help='verbose mode', action='store_true')
 	args = parser.parse_args()
 
-	debug=0
-	multicast_endpoint = Endpoint(port=args.port,hostname=args.address)
-	sys.stderr.write("Setting up sender (%s:%d)\n"%(multicast_endpoint.hostname,multicast_endpoint.port))
-	s = MulticastSocket(multicast_endpoint,bind_single=True,debug=debug)
-	running = True
-
-	ms_period = args.ms_period
-
-	ser = serial.Serial(port='COM3',baudrate=115200, timeout=10)
-	print "Resetting device to period of %d ms."%(ms_period)
-	writeCommand(ser, 'S')
-	writeCommand(ser, 'R')
-
-	bt = [(ms_period >> 8) & 0x00FF, (ms_period) & 0x00FF ]
-	writeCommand(ser, bt)
-	ret = ser.readline()
-
-	print "Starting."
-	writeCommand(ser, 'B')
-	running = True
-	while running:
-		try:
-			ret = ser.readline()
-			p = Packet("f"+ret,multicast_endpoint)
-			s.sendPacket(p)
-		except KeyboardInterrupt:
-			running = False
-
-	print "Stopping."
-	writeCommand(ser, 'S')
-	print "Goodbye."	
-	ser.close()
+	f = FreqServer(args.port,args.address,args.ms_period,verbose=args.verbose)
+	f.start(autojoin=False)
+	print "started"
+	time.sleep(5)
+	print "updating"
+	f.updatePeriod(500)
+	f.close()
