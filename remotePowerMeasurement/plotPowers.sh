6e61f1cd45b5a09e4a70cb21f14283ea9dd080f1
Josiah <jcmcclurg@gmail.com>
2015-10-22 11:51:44 -0500

    Updated plotPowers.sh
    Former-commit-id: aa311895e2e81154f02b7c8e18f915d7e4bad204

--- remotePowerMeasurement/plotPowers.sh
+++ remotePowerMeasurement/plotPowers.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-dir=$(dirname $0)
+dir=$( dirname $0 )
+defAddr='http://192.168.1.200:8282'
+defGroup='224.1.1.1'
+defPort=9995
+addr=${1:-$defAddr}
+group=${2:-$defGroup}
+port=${3:-$defPort}
 
-echo "Make sure you've initialized the streaming."
-#$dir/initializeStreaming.sh
+s=$(wget -O - "$addr/stream?command=stop&address=$group&port=$port" --quiet)
+echo "Stop stream returned ($s)"
+
+s=$(wget -O - "$addr/stream?command=start&length=10000&blockLength=1000&type=power&fields=34567&delimiter=%20&address=$group&port=$port" --quiet)
+echo "Start stream returned ($s)"
 
 cmd="$dir/feedgnuplot/bin/feedgnuplot --line --stream 0.1 --xlen 120"
 
-# Two windows: One for the cluster power and one for the individual powers
-#$dir/streamPowers.sh | tee >(stdbuf -oL -eL cut -d \  -f 3 | $cmd --legend 0 'whole cluster') | stdbuf -oL -eL cut -d \  -f 4,5,6,7,8 | $cmd --legend 0 'jjpowerserver' --legend 1 'jjpowerserver1' --legend 2 'jjpowerserver2' --legend 3 'jjpowerserver3' --legend 4 'jjpowerserver4'
-
-# One window for the invidiual powers
-#$dir/streamPowers.sh | stdbuf -oL -eL cut -d \  -f 4,5,6,7,8 | $cmd --legend 0 'jjpowerserver' --legend 1 'jjpowerserver1' --legend 2 'jjpowerserver2' --legend 3 'jjpowerserver3' --legend 4 'jjpowerserver4'
-
-$dir/streamPowerFast.sh | stdbuf -oL cut -d \  -f 4,5,6,7,8 | $cmd --legend 0 'jjpowerserver0' --legend 1 'jjpowerserver1' --legend 2 'jjpowerserver2' --legend 3 'jjpowerserver3' --legend 4 'jjpowerserver4'
+python -u $dir/measurementServer/multicast_listen.py -n -a $group -p $port | $cmd --legend 0 'jjpowerserver0' --legend 1 'jjpowerserver1' --legend 2 'jjpowerserver2' --legend 3 'jjpowerserver3' --legend 4 'jjpowerserver4'
