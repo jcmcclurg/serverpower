0724aba4f0048bc6836819b65ec5d3964d3a7c06
Josiah <jcmcclurg@gmail.com>
2015-10-22 11:52:36 -0500

    Removed unnecessary and confusing remote power measurement scripts
    Former-commit-id: 10d1390b56409e8989cba8675ec8a8c489e5940c [formerly 10d1390b56409e8989cba8675ec8a8c489e5940c [formerly 2832ff92576ebffd950eb4fe2d231ea1c6f5e397]]
    Former-commit-id: 725d3c0284c46f3fb0272019a8559f06fd27440a
    Former-commit-id: bc89fa0eb2776c73645324a93ebf0ce0aff42c97

--- remotePowerMeasurement/stopStreaming.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir=$( dirname $0 )
-defAddr='http://192.168.1.200:8282'
-
-addr=${1:-$defAddr}
-
-s=$(wget -O - "$addr/stream?command=stop" --quiet)
-echo "Stop stream returned ($s)"
--- remotePowerMeasurement/streamFrequency.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dir="/home/powerserver/joe/serverpower/remotePowerMeasurement"
-python -u $dir/measurementServer/multicast_listen.py -n -p 9998 -a 224.1.1.2
