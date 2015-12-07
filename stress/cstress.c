8168c46b4749c603f82dc473393143d47848a646
Josiah McClurg <jcmcclurg@gmail.com>
2015-12-07 17:55:19 -0600

    Re-wrote cstress so that it allows for accurate duty cycle and iteration counting
    Former-commit-id: c50389c987933513b57f8057c197ca7e73524ba7

--- stress/cstress.c
+++ stress/cstress.c
@@ -1,162 +1,301 @@
+/*
+Copyright (c) 2012, Intel Corporation
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+	* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+	* Neither the name of Intel Corporation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* Written by Martin Dimitrov, Carl Strickland
+Modified summer 2015 by Josiah McClurg
+*/
+
+#include <aio.h>
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-#include <math.h>
-#include <stdbool.h>
-#include <sys/select.h>
-#include <sys/time.h>
-#include <sys/types.h>
+#include <assert.h>
 #include <unistd.h>
 #include <string.h>
-#include <errno.h>
+#include <math.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <time.h>
+#include <signal.h>
 
-#define MAXDUTY 0.999
-#define MINDUTY 0.001
-#define DEFAULT_NUM_ITERATIONS ((long) 2000000)
-#define DEFAULT_DUTY ((double) MAXDUTY)
+#define CLOCKID CLOCK_REALTIME
 
+#define SIG_INT SIGINT
+#define SIG_TIMER SIGRTMIN
+#define SIG_AIO (SIGRTMIN+1)
+
+#define errExit(msg)	do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#define DEFAULT_DUTY 0.5
+#define DEFAULT_PERIOD 0.01
+
+#define STATE_INPUT_READY 3
+#define STATE_FINISHED 2 
+#define STATE_WORKING 1
+#define STATE_SLEEPING 0
+
+#define BUFSIZE 128
+
+char* progname;
+char* id;
+char* defaultId = "iterations:";
+
+struct sigaction aio_action;
+char stdin_buf[BUFSIZE];
+struct aiocb my_aiocb;
+
+struct sigaction timer_action;
 double duty;
-long num_iterations;
+double period;
 char verbose;
-char* cmd_name;
 
-void usage(void){
-	fprintf(stdout,"%s -h   print help\n",cmd_name);
-	fprintf(stdout,"   -n   [number of iterations (default: %ld]\n", DEFAULT_NUM_ITERATIONS);
-	fprintf(stdout,"   -v   verbose\n");
-	fprintf(stdout,"   -d   [initial duty (default: %lf)]\n", DEFAULT_DUTY);
+struct itimerspec timer_on;
+struct itimerspec timer_off;
+
+struct sigaction sigint_action;
+char sigint_count;
+
+timer_t timerid;
+char state;
+
+
+void usage(){
+	fprintf(stdout, "\nUsage: %s \n", progname);
+	fprintf(stdout, "   -v    verbose\n");
+	fprintf(stdout, "   -p    modulation period in seconds default %g\n",DEFAULT_PERIOD);
+	fprintf(stdout, "   -d    initial duty cycle (0 - 1) default %g\n",DEFAULT_DUTY);
+	fprintf(stdout, "\nType a number to change the duty cycle.\n");
+	fprintf(stdout, "Type p followed by a number to change the period.\n");
+	fprintf(stdout, "\n");
 }
 
-int cmdline(int argc, char** argv){
-	num_iterations = -1;
-	int i;
-	char opt = 0;
-	num_iterations = DEFAULT_NUM_ITERATIONS;
-	duty = DEFAULT_DUTY;
-	verbose = 0;
-	cmd_name = (char*) argv[0];
+void _update_duty(void){
+	double ont = duty*period;
+	double offt = (1.0-duty)*period;
 
-	if(argc == 2 && argv[1][0] != '-'){
-		num_iterations = atol(argv[1]);
+	timer_on.it_value.tv_sec = (time_t) ont;
+	timer_on.it_value.tv_nsec = (long) ((ont - ((double) timer_on.it_value.tv_sec))*1.0e9);
+	timer_off.it_value.tv_sec = (time_t) offt;
+	timer_off.it_value.tv_nsec = (long) ((offt - ((double) timer_off.it_value.tv_sec))*1.0e9);
+
+	// Make sure the timer actually goes, even if the duty/period calculations don't work out right.
+	if (timer_on.it_value.tv_sec == 0 && timer_on.it_value.tv_nsec <= 0){
+		timer_on.it_value.tv_nsec = 1;
 	}
-	else{
-		for(i = 1; i < argc; i++){
-			if(argv[i][0] == '-'){
-				opt = argv[i][1];
-				if(opt == 'h'){
-					usage();
-					return -1;
-				}
-				else if(opt == 'v'){
-					verbose = 1;
-				}
-				else if(opt == 'n' && i+1 < argc){
-					num_iterations = atol(argv[i+1]);
-				}
-				else if(opt == 'd' && i+1 < argc){
-					duty = atof(argv[i+1]);
-				}
-				else{
-					usage();
-					return -1;
-				}
-			}
-			else if(opt == 0){
-				usage();
-				return -1;
-			}
+	if (timer_off.it_value.tv_sec == 0 && timer_off.it_value.tv_nsec <= 0){
+		timer_off.it_value.tv_nsec = 1;
+	}
+}
+
+// Returns 0 on success, -1 on clipping at zero.
+char set_period(double p){
+	char r = 0;
+	if(p < 0.0){
+		p = 0.0;
+		r = -1;
+	}
+	period = p;
+	_update_duty();
+	return r;
+}
+
+// Returns 0 on success, -1 on clipping at zero, 1 on clipping at 1.
+char set_duty(double d){
+	char r = 0;
+	if(d < 0.0){
+		d = 0.0;
+		r = -1;
+	}
+	else if(d > 1.0){
+		d = 1.0;
+		r = 1;
+	}
+	duty = d;
+	_update_duty();
+	return r;
+}
+
+int cmdline(int argc, char **argv){
+	duty = DEFAULT_DUTY;
+	period = DEFAULT_PERIOD;
+	verbose = 0;
+	progname = argv[0];
+	id = defaultId;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "i:p:d:v")) != -1) {
+		switch (opt) {
+		case 'i':
+			id = optarg;
+		case 'p':
+			set_period(atof(optarg));
+			break;
+		case 'd':
+			set_duty(atof(optarg));
+			break;
+		case 'v':
+			verbose = 1;
+			break;
+		case 'h':
+			usage();
+			exit(0);
+			break;
+		default:
+			usage();
+			return -1;
 		}
 	}
-	
+	_update_duty();
 	return 0;
 }
 
-int main (int argc, char** argv) {
-	useconds_t sleeplen;
-	int num_readable;
-	long i;
-	double s;
-	int fd_stdin = fileno(stdin);
-	struct timeval tv;
-	tv.tv_sec = 0;
-	tv.tv_usec = 0;
-	double slen = 0;
-	char buf[128];
-	fd_set readfds;
-	double prevTime_sec, currentTime_sec;
-	struct timespec currentTime;
-	long num_repeats;
+static void sigtimer_handler(int sig, siginfo_t *si, void *uc) {
+	if(state == STATE_WORKING){
+		state = STATE_SLEEPING;
+		if (timer_settime(timerid, 0, &timer_off, NULL) == -1)
+			errExit("timer_settime1");
+	}
+	else{ // if state == STATE_SLEEPING
+		state = STATE_WORKING;
+		if (timer_settime(timerid, 0, &timer_on, NULL) == -1)
+			errExit("timer_settime2");
+	}
+}
 
-	// Clear output buffering on STDOUT
+void start_timer(void){
+	struct sigevent sev;
+
+	/* Establish handler for timer signal */
+	timer_action.sa_flags = SA_SIGINFO;
+	timer_action.sa_sigaction = sigtimer_handler;
+	sigemptyset(&timer_action.sa_mask);
+	if (sigaction(SIG_TIMER, &timer_action, NULL) == -1)
+		errExit("sigaction");
+
+	/* Create the timer */
+	sev.sigev_notify = SIGEV_SIGNAL;
+	sev.sigev_signo = SIG_TIMER;
+	sev.sigev_value.sival_ptr = &timerid;
+	if (timer_create(CLOCKID, &sev, &timerid) == -1)
+		errExit("timer_create");
+
+	/* Start the timer */
+	state = STATE_WORKING;
+
+	if (timer_settime(timerid, 0, &timer_on, NULL) == -1)
+		errExit("timer_settime");
+}
+
+void sigaio_handler(int signum, siginfo_t *info, void* uap){
+	char* b = (char*) my_aiocb.aio_buf;
+	if(verbose)
+		fprintf(stderr,"Got input: %s", b);
+	double d;
+	if(sscanf(b, "%lf", &d) > 0)
+		set_duty(d);
+
+	else if(b[0] == 'p' && sscanf(b+1,"%lf", &d) > 0)
+		set_period(d);
+
+	else if(b[0] == 'q')
+		state = STATE_FINISHED;
+
+	else if(verbose)
+		fprintf(stderr,"Could not parse.\n");
+
+	aio_read(&my_aiocb);
+}
+
+void start_io(void){
+	/* Create the handler for aio events */
+	aio_action.sa_sigaction = sigaio_handler;
+	aio_action.sa_flags = SA_SIGINFO;
+	sigemptyset(&aio_action.sa_mask);
+	sigaction(SIG_AIO, &aio_action, NULL);
+
+	/* Zero out the structure. */
+	memset(&stdin_buf,0,sizeof(stdin_buf));
+	memset(&my_aiocb,0,sizeof(my_aiocb));
+
+	/* Allocate a data buffer for the aiocb request */
+	my_aiocb.aio_buf = stdin_buf;
+
+	/* Initialize the necessary fields in the aiocb */
+	my_aiocb.aio_fildes = fileno(stdin);
+	my_aiocb.aio_nbytes = BUFSIZE;
+	my_aiocb.aio_offset = 0;
+	
+	my_aiocb.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+	my_aiocb.aio_sigevent.sigev_signo = SIG_AIO;
+	my_aiocb.aio_sigevent.sigev_value.sival_int = 0;
+	aio_read(&my_aiocb);
+}
+
+static void sigint_handler(int sig, siginfo_t *si, void *uc) {
+	if(sigint_count < 2){
+		state = STATE_FINISHED;
+		sigint_count++;
+	}
+	else
+		errExit("sigintcount");
+}
+
+void start_sigint(void){
+	sigint_count = 0;
+	sigint_action.sa_flags = SA_SIGINFO;
+	sigint_action.sa_sigaction = sigint_handler;
+	sigemptyset(&sigint_action.sa_mask);
+	if (sigaction(SIG_INT, &sigint_action, NULL) == -1)
+		errExit("sigaction");
+}
+
+int main(int argc, char **argv){
+
+	/* don't buffer if piped */
 	setbuf(stdout, NULL);
 	setbuf(stderr, NULL);
 
-	if(cmdline(argc,argv)){
-		exit(EXIT_FAILURE);
-	}
+	/* Process command line arguments */
+	cmdline(argc, argv);
 
-	if(verbose) fprintf(stderr,"Set duty to %lf\n",duty);
-	slen = 0;
-	sleeplen = 0;
-	if(clock_gettime(CLOCK_BOOTTIME, &currentTime)){
-		fprintf(stderr,"Problem with gettime\n");
-		exit(EXIT_FAILURE);
-	}
-	prevTime_sec = ((double)(currentTime.tv_sec)) + (((double)(currentTime.tv_nsec))/1.0e9);
+	/* Cleanly handle Ctrl+C */
+	start_sigint();
+	start_io();
+	start_timer();
 
-	num_repeats = 0;
-	while(1) {
-		FD_ZERO(&readfds);
-		FD_SET(fd_stdin, &readfds);
-		num_readable = select(fd_stdin +1, &readfds, NULL, NULL, &tv);
-		if (num_readable == 0) {
-			for(i = 0; i < num_iterations; i++){
-				s = sqrt((double) i);
+	double r;
+	double l;
+	int i;
+	long long iterations = 0;
+	while(state != STATE_FINISHED){
+		//if(verbose && state == STATE_WORKING)
+		//	fprintf(stderr,"Working\n");
+		while(state == STATE_WORKING){
+			l = 0;
+			for(i = 0; i < 1024; i++){
+				r = sqrt(l++);
 			}
-			num_repeats++;
-			if(clock_gettime(CLOCK_BOOTTIME, &currentTime)){
-				fprintf(stderr,"Problem with gettime\n");
-				exit(EXIT_FAILURE);
-			}
-			currentTime_sec = ((double)(currentTime.tv_sec)) + (((double)(currentTime.tv_nsec))/1.0e9);
-			slen = (1.0-duty)*(currentTime_sec - prevTime_sec);
-			sleeplen = (useconds_t)(slen*1.0e6);
-			//fprintf(stderr,"%lf...\n",slen);
-			usleep(sleeplen);
-			prevTime_sec = currentTime_sec;
+			iterations++;
 		}
-		else if (num_readable == 1) {
-			if(scanf("%lf",&duty) > 0){
-				if(duty > MAXDUTY){
-					duty = MAXDUTY;
-				}
-				else if(duty < MINDUTY){
-					duty = MINDUTY;
-				}
-				if(verbose) fprintf(stderr,"Set duty to %lg (previous sleeplen was %lg)\n",duty, slen);
-			}
-			else if(scanf("%s",buf) > 0 ){
-				if(buf[0] == 'q'){
-					if(verbose) fprintf(stderr,"Quitting\n");
-					break;
-				}
-				else{
-					fprintf(stderr,"Quit on scanf\n");
-					exit(EXIT_FAILURE);
-				}
-			}
-			else{
-				fprintf(stderr,"Failed on scanf\n");
-				exit(EXIT_FAILURE);
-			}
+		//if(verbose && state == STATE_SLEEPING)
+		//	fprintf(stderr,"Sleeping\n");
+		while(state == STATE_SLEEPING){
+			pause();
 		}
-		else{
-			fprintf(stderr,"Quit on select\n");
-			exit(EXIT_FAILURE);
-		}
+		//if(verbose && state == STATE_FINISHED)
+		//	fprintf(stderr,"Finished\n");
 	}
-	fprintf(stdout,"Ran %ld iterations of %ld sqrt operations.\n",num_repeats,num_iterations);
-
+	if(verbose)
+		fprintf(stderr,"Goodbye.\n");
+	fprintf(stdout,"%s%lld\n",id,iterations);
 	exit(EXIT_SUCCESS);
-	return (int) s;
+	return (int) r;
 }
