a419894dae0fa927d54386f9a62f8fce2abbdad4
Josiah McClurg <jcmcclurg@gmail.com>
2015-12-08 00:40:09 -0600

    updated record_stress to use the playback
    Former-commit-id: b234ab1f93a0dc7003a0acc9db2fd7c11b02c585

--- stress/record_stress.sh
+++ stress/record_stress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dir="./power_output"
-d=`date +%N_%s`
-echo $d
+dir=$(dirname $0)
+p=0.01
+s=$dir/cstress
 
-./all_stress.sh 2>"$dir/$d"".log" >"$dir/$d"".output"
+( ( $dir/../utilities/playback -f $dir/../utilities/ramp_0_1.csv; echo "q"; echo "q") | tee >($s -i 1: -p $p) >($s -i 2: -p $p) >($s -i 3: -p $p) >($s -i 4: -p $p) >($s -i 5: -p $p) >($s -i 6: -p $p) >($s -i 7: -p $p) >($s -i 8: -p $p) >($s -i 9: -p $p) >($s -i 10: -p $p) >($s -i 11: -p $p) >($s -i 12: -p $p) > /dev/null ) 2>&1
