20030c5d9aaca4339414acd1f010d9ebc5ea3eca
Josiah <jcmcclurg@gmail.com>
2015-12-08 20:55:56 -0600

    Updated multicast listen to have a logging option

--- remotePowerMeasurement/measurementServer/multicast_listen.py
+++ remotePowerMeasurement/measurementServer/multicast_listen.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Listens to a multicast address and prints the packet data to standard output.')
 	parser.add_argument('-p', '--port', type=int, help='the port on which to listen', default=9999)
 	parser.add_argument('-a', '--address', help='the address on which to listen', default='224.1.1.1')
+	parser.add_argument('-l', '--logfile', help='write to logfile instead of standard out', default='')
 	parser.add_argument('-n', '--nonewline', help='do not print a newline after the packet data', action='store_true')
 	parser.add_argument('-s', '--size', type=int, help='maximum size of packets', default=1024)
 	parser.add_argument('-t', '--timestamp', help='print the timestamp before the data', action='store_true')
@@ -24,7 +25,16 @@ if __name__ == "__main__":
 	multicast_endpoint = Endpoint(port=args.port,hostname=args.address)
 	if args.verbose:
 		sys.stderr.write("Setting up listener (%s:%d)\n"%(multicast_endpoint.hostname,multicast_endpoint.port))
-		sys.stderr.write("Options are nonewline=%s size=%d\n"%(args.nonewline,args.size))
+		sys.stderr.write("Options are nonewline=%s size=%d logfile=%s\n"%(args.nonewline,args.size, args.logfile))
+		sys.stderr.flush()
+	if args.logfile == '':
+		output = sys.stdout
+		output.flush()
+	else:
+		output = open(args.logfile,'wb')
+		output.flush()
+		#output.truncate()
+
 	s = MulticastSocket(multicast_endpoint,bind_single=False,debug=debug)
 	running = True
 	while running:
@@ -33,14 +43,21 @@ if __name__ == "__main__":
 			t = time.time()
 			if args.verbose:
 				sys.stderr.write("Received packet %s\n"%(p))
+				sys.stderr.flush()
 			if args.timestamp:
-				sys.stdout.write("%f:"%(t))
+				output.write("%f:"%(t))
 			if args.nonewline:
-				sys.stdout.write(p.data)
+				output.write(p.data)
+				output.flush()
 			else:
-				sys.stdout.write(p.data+"\n")
+				output.write(p.data+"\n")
+				output.flush()
 		except (KeyboardInterrupt, ValueError, IOError) as e:
 			running = False
 
 	s.close()
 	sys.stderr.write("Goodbye.\n")
+	sys.stderr.flush()
+	if not (args.logfile == ''):
+		output.flush()
+		output.close()
