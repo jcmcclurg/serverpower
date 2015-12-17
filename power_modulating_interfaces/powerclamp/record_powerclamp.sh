77d9e11a513c39dedc2f43898151e669904eadee
Josiah McClurg <jcmcclurg@gmail.com>
2015-12-17 11:26:36 -0600

    Updated record_powerclamp sh
    Former-commit-id: 12d5576a9ef0759a6d8a3c1e03dceccd0bff9f19 [formerly 12d5576a9ef0759a6d8a3c1e03dceccd0bff9f19 [formerly 2663ebd439d13d665a8111b14818c5f6091e0362]]
    Former-commit-id: c398370b722a34bacf02991a866386b76ca90164
    Former-commit-id: 347ede14156fdf479bf9a20ef5aee26790312f96

--- power_modulating_interfaces/powerclamp/record_powerclamp.sh
+++ power_modulating_interfaces/powerclamp/record_powerclamp.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-dir="./power_output"
-d=`date +%N_%s`
-echo $d
-#./stress_insertDelays.sh 2>"$dir/$d"".log" >"$dir/$d"".output"
+dir=$(dirname $0)
+p=0.01
+s=$dir/../stress/cstress
 
-./all_powerclamp.sh 2>"$dir/$d"".log" >"$dir/$d"".output"
+playbackCmd="$dir/../../utilities/playback/playback"
+powerClamp="$dir/powerclamp.sh"
+defRampFile="$dir/../../utilities/playback/ramp_0_50.csv"
+dummyRampFile="$dir/../../utilities/playback/ramp_0_50_short.csv"
+
+rampFile=${1-$defRampFile}
+
+if [[ "x$rampFile" == "xdummy" ]]; then
+	rampFile=$dummyRampFile
+fi
+
+numCPUs=12
+
+for i in $(seq 1 $numCPUs); do
+	$s -i "$i:" -d 1 -p 10 2> "/tmp/powerclamp_worker_${i}.log" & pids[$[ $i - 1 ]]=$!
+done
+
+# -w: period of 10 ms
+# -d: initial duty of 0.5
+# -u: do not update PID list
+# -U: except if there is an error sending signals to the PIDs
+# -o: do not try to find or control any children of the specified PIDs
+# -p: control the specified PIDs
+(($playbackCmd -v -f $rampFile; echo "q"; echo "q"; ) | sudo $powerClamp ) 2>&1
+
+echo ${pids[*]}
+
+for pid in ${pids[*]}; do
+	echo "Killing $pid" >&2
+	pkill --signal INT -P $pid
+	kill -s INT $pid
+done
+
+wait
