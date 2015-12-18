75eb15d5409a58839e04f6e398aa75c41d64b8ef
Josiah McClurg <jcmcclurg@gmail.com>
2015-12-18 13:43:31 -0600

    Updated cpufreq

--- power_modulating_interfaces/cpufreq/record_cpufreq.sh
+++ power_modulating_interfaces/cpufreq/record_cpufreq.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
-dir="./power_output"
-d=`date +%N_%s`
-echo $d
-#./stress_insertDelays.sh 2>"$dir/$d"".log" >"$dir/$d"".output"
+dir=$(dirname $0)
+p=0.01
+s=$dir/../stress/cstress
 
-./all_cpufreq.sh 2>"$dir/$d"".log" >"$dir/$d"".output"
+playbackCmd="$dir/../../utilities/playback/playback"
+cpufreq="$dir/cpufreq.sh"
+
+read rangeMin rangeMax <<<$(sudo cpufreq-info -l)
+
+defRampFile="$dir/../../utilities/playback/ramp_${rangeMin}_${rangeMax}.csv"
+dummyRampFile="$dir/../../utilities/playback/ramp_${rangeMin}_${rangeMax}_short.csv"
+
+rampFile=${1-$defRampFile}
+
+if [[ "x$rampFile" == "xdummy" ]]; then
+	rampFile=$dummyRampFile
+fi
+
+numCPUs=12
+
+for i in $(seq 1 $numCPUs); do
+	$s -i "$i:" -d 1 -p 10 2> "/tmp/cpufreq_worker_${i}.log" & pids[$[ $i - 1 ]]=$!
+done
+
+echo "RangeMin = $rangeMin"
+echo "RangeMax = $rangeMax"
+(($playbackCmd -v -f $rampFile; echo "q"; echo "q"; ) | sudo $cpufreq ) 2>&1
+
+echo ${pids[*]}
+
+for pid in ${pids[*]}; do
+	echo "Killing $pid" >&2
+	pkill --signal INT -P $pid
+	kill -s INT $pid
+done
+
+wait
