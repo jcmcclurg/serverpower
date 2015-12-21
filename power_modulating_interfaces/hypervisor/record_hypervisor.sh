ed5b737ff976d326fd5cdf1637107ddb8ce7a352
Josiah McClurg <jcmcclurg@gmail.com>
2015-12-21 16:06:25 -0600

    updated
    Former-commit-id: 4a79b700de8ea6e3d693ca28ffc64d231d14856b

--- power_modulating_interfaces/hypervisor/record_hypervisor.sh
+++ power_modulating_interfaces/hypervisor/record_hypervisor.sh
@@ -17,19 +17,16 @@ fi
 
 numCPUs=12
 
-sudo xl create /etc/xen/ubud1.cfg
-sleep 5
+if ! sudo xl list ubud1 >/dev/null 2>/dev/null; then
+	sudo xl create /etc/xen/ubud1.cfg
+	sleep 15
+fi
+
 ssh $(hostname)v "/bin/bash -c 'for i in \$(seq 1 $numCPUs); do $s -i \"\$i:\" -d 1 -p 10 2> \"/tmp/hypervisor_worker\${i}.log\" & echo \$! > /tmp/hypervisor_worker\${i}.pid; done'" & pid=$!
 
-# -w: period of 10 ms
-# -d: initial duty of 0.5
-# -u: do not update PID list
-# -U: except if there is an error sending signals to the PIDs
-# -o: do not try to find or control any children of the specified PIDs
-# -p: control the specified PIDs
 (($playbackCmd -v -f $rampFile; echo "q"; echo "q"; ) | sudo $hypervisor ) 2>&1
 
-ssh $(hostname)v "/bin/bash -c 'sudo poweroff'"
+ssh $(hostname)v "/bin/bash -c 'killall cstress'"
 
 #echo "Killing $pid" >&2
 #pkill --signal INT -P $pid
