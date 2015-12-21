9d2dd070a4813e194f0fa1799dfcef7c0653617b
Josiah McClurg <jcmcclurg@gmail.com>
2015-12-21 15:52:58 -0600

    Updated hypervisor code
    Former-commit-id: 2bf66cec0b8a29d2befec93c96d3e7daf45eab4b

--- power_modulating_interfaces/hypervisor/hypervisor.sh
+++ power_modulating_interfaces/hypervisor/hypervisor.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-echo Welcome to the hypervisor cpu limiter. >&2
-echo Type the limiting values. Press CTRL+C or q to exit. >&2
-echo The range is 0-400. See xl sched-credit -c for details >&2
-
 dom=${1:-ubud1}
-rangeMax=400
+rangeMax=1200
 rangeMin=1
 
-limitVal=400
+echo Welcome to the hypervisor cpu limiter. >&2
+echo Type the limiting values. Press CTRL+C or q to exit. >&2
+echo The range is $rangeMin-$rangeMax See xl sched-credit -c for details >&2
+
+val=1200
 echo "Limiting $dom." >&2
 
 trap "sudo xl sched-credit -d $dom -c 0; echo 'Reset credits to 0 on exit.' >&2; exit;" SIGINT SIGTERM
 
-while [[ "$limitVal" != "q" ]]; do
-	limitVal=$( printf '%.0f' $limitVal)
-	if [ $limitVal -lt $rangeMin ]; then
-		limitVal=$rangeMin
-	elif [ $limitVal -gt $rangeMax ]; then
-		limitVal=$rangeMax
+while [[ "$val" != "q" ]]; do
+	val=$( printf '%.0f' $val)
+	if [ $val -lt $rangeMin ]; then
+		val=$rangeMin
+	elif [ $val -gt $rangeMax ]; then
+		val=$rangeMax
 	fi
-	sudo xl sched-credit -d $dom -c $limitVal
-	read limitVal
-	#echo "Set credits to $limitVal" >&2
+	sudo xl sched-credit -d $dom -c $val
+	read val
+	#echo "Set credits to $val" >&2
 done
 sudo xl sched-credit -d $dom -c 0
 echo "Done" >&2
