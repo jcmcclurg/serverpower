4f85d9fd6263f75fb79f622638e978aed70448a6
Josiah on jjpowerserver1 <jcmcclurg@gmail.com>
2016-02-25 11:58:11 -0600

    Updated record_cgroups.sh

--- power_modulating_interfaces/cgroups/record_cgroups.sh
+++ power_modulating_interfaces/cgroups/record_cgroups.sh
@@ -5,10 +5,10 @@ p=0.01
 s=$dir/../stress/cstress
 
 playbackCmd="$dir/../../utilities/playback/playback"
-powerClamp="$dir/powerclamp.sh"
+cgroups="$dir/cgroup.sh"
 
-rangeMin=0
-rangeMax=50
+rangeMin=12000
+rangeMax=1200000
 defRampFile="$dir/interleavedRamp_${rangeMin}_${rangeMax}.csv"
 dummyRampFile="$dir/interleavedRamp_${rangeMin}_${rangeMax}_short.csv"
 
@@ -24,9 +24,15 @@ fi
 
 numCPUs=12
 
+echo "Starting workers..." >&2
 for i in $(seq 1 $numCPUs); do
-	$s -i "$i:" -d 1 -p 10 2> "/tmp/powerclamp_worker_${i}.log" & pids[$[ $i - 1 ]]=$!
+	$s -i "$i:" -d 1 -p 10 2> "/tmp/cgroup_worker_${i}.log" & pid=$!
+	pids[$[ $i - 1 ]]=$pid
+	echo $pid > /sys/fs/cgroup/cpu/jgroup/cgroup.procs
 done
+
+echo "Cgroup contains: " >&2
+cat /sys/fs/cgroup/cpu/jgroup/cgroup.procs >&2
 
 # -w: period of 10 ms
 # -d: initial duty of 0.5
@@ -34,7 +40,7 @@ done
 # -U: except if there is an error sending signals to the PIDs
 # -o: do not try to find or control any children of the specified PIDs
 # -p: control the specified PIDs
-(($playbackCmd -v -f $rampFile; echo "q"; echo "q"; ) | sudo $powerClamp ) 2>&1
+(($playbackCmd -v -f $rampFile; echo "q"; echo "q"; ) | $cgroups ) 2>&1
 
 echo ${pids[*]}
 
