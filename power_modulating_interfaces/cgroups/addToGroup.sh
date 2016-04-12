1591fc03aab7ab644254d1c44e5cea92947ae087
Josiah on jjpowerserver1 <jcmcclurg@gmail.com>
2016-04-12 09:13:40 -0500

    base path bug

--- power_modulating_interfaces/cgroups/addToGroup.sh
+++ power_modulating_interfaces/cgroups/addToGroup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+dir=$(dirname $0)
 echo "Adding $@ and its children." >&2
-../../utilities/getChildPIDs/getChildPIDs.sh $@ | while read line; do
+$dir/../../utilities/getChildPIDs/getChildPIDs.sh $@ | while read line; do
 	echo "   adding $line" >&2
 	echo "$line" > /sys/fs/cgroup/cpu/jgroup/cgroup.procs
 done
