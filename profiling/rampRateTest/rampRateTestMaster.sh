51308c9de6695d44668edf5e1fae006ec6092336
Josiah <jcmcclurg@gmail.com>
2016-07-04 09:09:04 -0500

    Updated ramp rate test

--- profiling/rampRateTest/rampRateTestMaster.sh
+++ profiling/rampRateTest/rampRateTestMaster.sh
@@ -3,13 +3,13 @@
 dir=$( realpath $( dirname $0 ) )
 
 ip="192.168.1.200"
-logfile=$(cygpath -w "$dir/powerlog.log")
+logfile=$(cygpath -w "$dir/powerlog3.log")
 
 echo "Cleaning up old power streams..." >&2
 wget -O - "http://$ip:8282/stream?command=stop&address=$logfile" --quiet
 
 echo "Starting power stream..." >&2
-wget -O - "http://$ip:8282/stream?command=start&length=10000&type=csvScaled&fields=01234567&address=$logfile" --quiet
+wget -O - "http://$ip:8282/stream?command=start&length=10000&blockLen&type=csvScaled&fields=01234567&address=$logfile" --quiet
 
 remotecmd="/bin/bash -c '/home/josiah/serverpower/profiling/rampRateTest/rampRateTestClient.sh'"
 
@@ -20,7 +20,7 @@ ssh josiah@192.168.1.104 "$remotecmd" & testPID[3]=$!
 
 echo "Test PIDs are ${testPID[*]}. Waiting until they finish." >&2
 
-$dir/../../remotePowerMeasurement/measurementServer/multicast_send.py < <(sleep 5; echo 't'; sleep 5; echo 't'; sleep 5; echo 's')
+$dir/../../remotePowerMeasurement/measurementServer/multicast_send.py < <(sleep 1; echo 't'; sleep 3; echo 't'; sleep 3; echo 't'; sleep '3'; echo 't'; sleep 3; echo 's')
 
 wait ${testPID[0]}; echo "Test 1 finished."
 wait ${testPID[1]}; echo "Test 2 finished."
