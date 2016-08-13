cee38678cb1030b870c055e389f18e43baf1af88
Josiah on jjpowerserver0 <jcmcclurg@gmail.com>
2016-08-13 08:36:38 -0500

    Updated turbostat

--- utilities/turbostat/turbostat_josiah.c
+++ utilities/turbostat/turbostat_josiah.c
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ * Modified 8/13/2016 by Josiah McClurg (added timestamp feature)
  */
 
 #define _GNU_SOURCE
