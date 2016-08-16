1d706ef5b904edf4336db26fa890bd0d0dd7a8e9
Josiah on jjpowerserver0 <jcmcclurg@gmail.com>
2016-08-16 11:49:12 -0500

    Added turbostat runner

---
+++ utilities/turbostat/tsRunner.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+dir=$(dirname $0)
+
+turboStat="$dir/turbostat_josiah"
+turboStatOut="/tmp/turbostat.out"
+turboStatErr="/tmp/turbostat.err"
+turboStatPIDFile="/tmp/turbostat.pid"
+
+streamListener="../multicast_tool/multicast_listen.py"
+listenerPort=9979
+
+currentCommand="none"
+echo "Started turbostat runner." >&2
+python -u $streamListener -p $listenerPort | while read cmd; do
+	if [ "x$cmd" != "x$currentCommand" ]; then
+		if [ "x$cmd" == "xstart" ]; then
+			echo "Starting turbostat..." >&2
+			rm $turboStatOut
+			sudo $turboStat -d -d -t -i 0.2 -S > $turboStatOut 2> $turboStatErr & tsPID=$!
+			echo "$tsPID" > $turboStatPIDFile
+			currentCommand="$cmd"
+		else
+			echo "Killing turbostat..." >&2
+			sudo kill $tsPID
+			wait
+			rm $turboStatPIDFile
+			echo "Turbostat killed." >&2
+			currentCommand="$cmd"
+		fi
+	fi
+	if [ "x$cmd" == "xquit" ]; then
+		break;
+	fi
+done
+
+echo "Quit turbostat runner." >&2
