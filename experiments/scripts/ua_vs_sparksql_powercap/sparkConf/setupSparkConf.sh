32a07d381d96791a37f994f6f41a1fd93137354f
Josiah <jcmcclurg@gmail.com>
2017-06-26 13:47:32 -0500

    updated sparkConf

--- experiments/scripts/ua_vs_sparksql_powercap/sparkConf/setupSparkConf.sh
+++ experiments/scripts/ua_vs_sparksql_powercap/sparkConf/setupSparkConf.sh
@@ -3,4 +3,10 @@
 dir=$( dirname "$BASH_SOURCE" )
 dir=$( readlink -fm "$dir" )
 
-sed "s|__HERE__|$dir|g" "$dir/spark-defaults.conf.template" > "$dir/spark-defaults.conf"
+if [ "$1" == "test" ]; then
+	template="$dir/spark-defaults-test.conf.template"
+else
+	template="$dir/spark-defaults.conf.template"
+fi
+
+sed "s|__HERE__|$dir|g" "$template" > "$dir/spark-defaults.conf"
